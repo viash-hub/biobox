2ae38b27137a0a9a61d2c643ccb10c3dd75fe94f
Robrecht Cannoodt <rcannood@gmail.com>
2024-01-26 16:06:07 +0100

    extend test

--- src/fastp/test.sh
+++ src/fastp/test.sh
@@ -7,6 +7,10 @@ meta_executable="target/docker/fastp/fastp"
 meta_resources_dir="src/fastp"
 ## VIASH END
 
+#########################################################################################
+mkdir fastp_se
+cd fastp_se
+
 echo "> Run fastp on SE"
 "$meta_executable" \
   --in1 "$meta_resources_dir/test_data/se/a.fastq" \
@@ -22,7 +26,28 @@ echo ">> Check if output exists"
 [ ! -f "report.json" ] && echo ">> report.json does not exist" && exit 1
 [ ! -f "report.html" ] && echo ">> report.html does not exist" && exit 1
 
-echo ">> Run fastp on PE"
+#########################################################################################
+cd ..
+mkdir fastp_pe_minimal
+cd fastp_pe_minimal
+
+echo ">> Run fastp on PE with minimal parameters"
+"$meta_executable" \
+  --in1 "$meta_resources_dir/test_data/pe/a.1.fastq" \
+  --in2 "$meta_resources_dir/test_data/pe/a.2.fastq" \
+  --out1 "trimmed_1.fastq" \
+  --out2 "trimmed_2.fastq"
+
+echo ">> Check if output exists"
+[ ! -f "trimmed_1.fastq" ] && echo ">> trimmed_1.fastq does not exist" && exit 1
+[ ! -f "trimmed_2.fastq" ] && echo ">> trimmed_2.fastq does not exist" && exit 1
+
+#########################################################################################
+cd ..
+mkdir fastp_pe_many
+cd fastp_pe_many
+
+echo ">> Run fastp on PE with many parameters"
 "$meta_executable" \
   --in1 "$meta_resources_dir/test_data/pe/a.1.fastq" \
   --in2 "$meta_resources_dir/test_data/pe/a.2.fastq" \
@@ -43,5 +68,7 @@ echo ">> Check if output exists"
 [ ! -f "report.json" ] && echo ">> report.json does not exist" && exit 1
 [ ! -f "report.html" ] && echo ">> report.html does not exist" && exit 1
 [ ! -f "merged.fastq" ] && echo ">> merged.fastq does not exist" && exit 1
+
+#########################################################################################
 
 echo "> Test successful"
