f02d9e3df70e422fdd38c2891b17d23e3236b584
Kai Waldrant <kai@data-intuitive.com>
2024-01-29 21:40:58 +0100

    update test_data

--- src/pear/test.sh
+++ src/pear/test.sh
@@ -6,14 +6,13 @@ dir_in="${meta_resources_dir%/}/test_data"
 
 echo "> Run PEAR"
 "$meta_executable" \
-  --forward_fastq "$dir_in/reads.left.fq.gz" \
-  --reverse_fastq "$dir_in/reads.right.fq.gz" \
+  --forward_fastq "$dir_in/a.1.fastq" \
+  --reverse_fastq "$dir_in/a.2.fastq" \
   --output "test" \
   --p_value 0.01 \
   --memory "4G"
 
 echo ">> Checking output"
-echo $(ls)
 [ ! -f "test.assembled.fastq.gz" ] && echo "Output file test.assembled.fastq.gz does not exist" && exit 1
 [ ! -f "test.unassembled.forward.fastq.gz" ] && echo "Output file test.unassembled.forward.fastq.gz does not exist" && exit 1
 [ ! -f "test.unassembled.reverse.fastq.gz" ] && echo "Output file test.unassembled.reverse.fastq.gz does not exist" && exit 1
