41c092670ee90cb4930dcec5beaa73459768e478
dorien-er <roosen.dorien@gmail.com>
2024-01-30 21:22:38 +0100

    update test

--- src/busco/busco/test.sh
+++ src/busco/busco/test.sh
@@ -1,6 +1,6 @@
 test_dir="$meta_resources_dir/test_data"
 
-echo "> Running busco"
+echo "> Running busco with lineage dataset"
 
 "$meta_executable" \
     --input $test_dir/protein.fasta \
@@ -20,11 +20,12 @@ echo ">> Checking if output is empty"
 [ ! -s "output/short_summary.json" ] && echo "short_summary.json is empty" && exit 1
 [ ! -s "output/short_summary.txt" ] && echo "short_summary.txt is empty" && exit 1
 
+echo "> Running busco with auto lineage"
 
 "$meta_executable" \
     --input $test_dir/protein.fasta \
     --mode proteins \
-    --auto_lineage \
+    --auto_lineage_euk \
     --output_dir output 
 
 echo ">> Checking output"
