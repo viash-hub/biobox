eec3aad538e9718fa80d5c2a7d75eb6458e39a83
dorien-er <roosen.dorien@gmail.com>
2024-01-30 18:52:50 +0100

    update tests

--- src/busco/busco/test.sh
+++ src/busco/busco/test.sh
@@ -10,16 +10,12 @@ echo "$(busco --version 2>&1 | sed -n 's/BUSCO \([0-9.]*\)/\1/p')"
     --output_dir output 
 
 echo ">> Checking output"
-[ ! -f "output/short_summary.specific.stramenopiles_odb10.protein.fasta.json" ] && echo "specific_short_summary.json does not exist" && exit 1
-[ ! -f "output/short_summary.specific.stramenopiles_odb10.protein.fasta.txt" ] && echo "specific_short_summary.txt does not exist" && exit 1
 [ ! -f "output/run_stramenopiles_odb10/full_table.tsv" ] && echo "full_table.tsv does not exist" && exit 1
 [ ! -f "output/run_stramenopiles_odb10/missing_busco_list.tsv" ] && echo "missing_busco_list.tsv does not exist" && exit 1
 [ ! -f "output/run_stramenopiles_odb10/short_summary.json" ] && echo "short_summary.json does not exist" && exit 1
 [ ! -f "output/run_stramenopiles_odb10/short_summary.txt" ] && echo "short_summary.txt does not exist" && exit 1
 
 echo ">> Checking if output is empty"
-[ ! -s "output/short_summary.specific.stramenopiles_odb10.protein.fasta.json" ] && echo "specific_short_summary.json is empty" && exit 1
-[ ! -s "output/short_summary.specific.stramenopiles_odb10.protein.fasta.txt" ] && echo "specific_short_summary.txt is empty" && exit 1
 [ ! -s "output/run_stramenopiles_odb10/full_table.tsv" ] && echo "full_table.tsv is empty" && exit 1
 [ ! -s "output/run_stramenopiles_odb10/missing_busco_list.tsv" ] && echo "missing_busco_list.tsv is empty" && exit 1
 [ ! -s "output/run_stramenopiles_odb10/short_summary.json" ] && echo "short_summary.json is empty" && exit 1
