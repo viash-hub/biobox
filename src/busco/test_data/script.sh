f7acecc47868b1639585e03ba8cddc8a92436181
dorien-er <roosen.dorien@gmail.com>
2024-01-30 17:01:56 +0100

    merge

--- src/busco/test_data/script.sh
+++
@@ -1,9 +0,0 @@
-# busco test data
-
-# Test data was obtained from https://github.com/snakemake/snakemake-wrappers/tree/master/bio/busco/test
-
-if [ ! -d /tmp/snakemake-wrappers ]; then
-  git clone --depth 1 --single-branch --branch master https://github.com/snakemake/snakemake-wrappers /tmp/snakemake-wrappers
-fi
-
-cp -r /tmp/snakemake-wrappers/bio/busco/test/protein.fasta src/busco/test_data
