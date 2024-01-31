69c2b45cc244c07de4e4de4dd7047a6c03be40bd
Robrecht Cannoodt <rcannood@gmail.com>
2024-01-31 12:15:35 +0100

    rename files to verify that parameters work as intended

--- src/featurecounts/test.sh
+++ src/featurecounts/test.sh
@@ -8,9 +8,9 @@ echo "> Run featurCounts"
 "$meta_executable" \
   --input "$dir_in/a.bam" \
   --annotation "$dir_in/annotation.gtf" \
-  --output_counts "features.txt" \
-  --output_summary "features.txt.summary" \
-  --output_junctions "features.txt.jcounts" \
+  --output_counts "features.tsv" \
+  --output_summary "summary.tsv" \
+  --output_junctions "junction_counts.txt" \
   --junctions \
   --ref_fasta "$dir_in/genome.fasta" \
   --overlapping \
@@ -19,13 +19,13 @@ echo "> Run featurCounts"
   --strand 0
 
 echo ">> Checking output"
-[ ! -f "features.txt" ] && echo "Output file features.txt does not exist" && exit 1
-[ ! -f "features.txt.summary" ] && echo "Output file features.txt.summary does not exist" && exit 1
-[ ! -f "features.txt.jcounts" ] && echo "Output file features.txt.jcounts does not exist" && exit 1
+[ ! -f "features.tsv" ] && echo "Output file features.tsv does not exist" && exit 1
+[ ! -f "summary.tsv" ] && echo "Output file summary.tsv does not exist" && exit 1
+[ ! -f "junction_counts.txt" ] && echo "Output file junction_counts.txt does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "features.txt" ] && echo "Output file features.txt is empty" && exit 1
-[ ! -s "features.txt.summary" ] && echo "Output file features.txt.summary is empty" && exit 1
-[ ! -s "features.txt.jcounts" ] && echo "Output file features.txt.jcounts is empty" && exit 1
+[ ! -s "features.tsv" ] && echo "Output file features.tsv is empty" && exit 1
+[ ! -s "summary.tsv" ] && echo "Output file summary.tsv is empty" && exit 1
+[ ! -s "junction_counts.txt" ] && echo "Output file junction_counts.txt is empty" && exit 1
 
 echo "> Test successful"
