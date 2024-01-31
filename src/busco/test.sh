699631f6af78cd226afd8b2598e4e8d411085d51
dorien-er <roosen.dorien@gmail.com>
2024-01-31 11:31:49 +0100

    fix typo

--- src/busco/test.sh
+++ src/busco/test.sh
@@ -13,7 +13,7 @@ echo "> Running busco with lineage dataset"
     --short_summary_json short_summary.json \
     --short_summary_txt short_summary.txt \
     --full_table full_table.tsv \
-    --missing_busco_list missing_list.tsv
+    --missing_busco_list missing_busco_list.tsv
 
 echo ">> Checking output"
 [ ! -f "output/full_table.tsv" ] && echo "full_table.tsv does not exist" && exit 1
@@ -61,3 +61,28 @@ echo ">> Checking if output is empty"
 
 rm -r output/ 
 
+cd ..
+mkdir "run_geno_autolineage"
+cd "run_geno_autolineage"
+
+echo "> Running busco with auto lineage"
+
+"$meta_executable" \
+    --input $test_dir/genome.fasta \
+    --mode proteins \
+    --auto_lineage \
+    --output_dir output 
+
+echo ">> Checking output"
+[ ! -f "output/full_table.tsv" ] && echo "full_table.tsv does not exist in output folder" && exit 1
+[ ! -f "output/missing_busco_list.tsv" ] && echo "missing_busco_list.tsv does not exist in output folder" && exit 1
+[ ! -f "output/short_summary.json" ] && echo "short_summary.json does not exist in output folder" && exit 1
+[ ! -f "output/short_summary.txt" ] && echo "short_summary.txt does not exist in output folder" && exit 1
+
+echo ">> Checking if output is empty"
+[ ! -s "output/full_table.tsv" ] && echo "full_table.tsv in output folder is empty" && exit 1
+[ ! -s "output/missing_busco_list.tsv" ] && echo "missing_busco_list.tsv in output folder is empty" && exit 1
+[ ! -s "output/short_summary.json" ] && echo "short_summary.json in output folder is empty" && exit 1
+[ ! -s "output/short_summary.txt" ] && echo "short_summary.txt in output folder is empty" && exit 1
+
+rm -r output/ 
