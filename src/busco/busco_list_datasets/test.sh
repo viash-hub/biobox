a858cc1ecd3cb69b82160d298b93f2e12476d55d
dorien-er <roosen.dorien@gmail.com>
2024-02-06 15:46:14 +0100

    update test

--- src/busco/busco_list_datasets/test.sh
+++ src/busco/busco_list_datasets/test.sh
@@ -3,4 +3,11 @@
 ## VIASH START
 ## VIASH END
 
-busco --list-datasets > $par_output 
+"$meta_executable" \
+    --output datasets.txt 
+
+echo ">> Checking output"
+[ ! -f "datasets.txt" ] && echo "datasets.txt does not exist" && exit 1
+
+echo ">> Checking if output is empty"
+[ ! -s "datasets.txt" ] && echo "datasets.txt is empty" && exit 1
