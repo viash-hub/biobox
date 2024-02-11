51cd979700f55dd83d1b6976219fecf04f3c8368
Robrecht Cannoodt <rcannood@gmail.com>
2024-02-11 22:05:01 +0100

    better utf8 to ascii conversion

--- src/star/star_align/utils/process_params.R
+++ src/star/star_align/utils/process_params.R
@@ -9,7 +9,7 @@ yaml_file <- "src/star/star_align/argument_groups.yaml"
 param_txt <- readr::read_lines(local_file)
 
 # replace non-ascii characters with their ascii approximations
-param_txt <- iconv(param_txt, "UTF-8", "ASCII", sub = "byte")
+param_txt <- iconv(param_txt, "UTF-8", "ASCII//TRANSLIT")
 
 dev_begin <- grep("#####UnderDevelopment_begin", param_txt)
 dev_end <- grep("#####UnderDevelopment_end", param_txt)
