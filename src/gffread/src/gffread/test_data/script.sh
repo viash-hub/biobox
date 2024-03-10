d4c42c7de914135f02f804dd33f47c81df6733ba
emmarousseau <emmarou1@icloud.com>
2024-03-10 17:16:07 +0100

    Restructured directories and changed test data source and script

--- src/gffread/src/gffread/test_data/script.sh
+++
@@ -1,8 +0,0 @@
-
-# clone repo
-if [ ! -d /tmp/snakemake-wrappers ]; then
-  git clone --depth 1 --single-branch --branch master https://github.com/snakemake/snakemake-wrappers /tmp/snakemake-wrappers
-fi
-
-# copy test data
-cp -r /tmp/snakemake-wrappers/bio/gffread/test/* src/gffread/test_data
---
+++ src/gffread/test_data/script.sh
@@ -0,0 +1,8 @@
+
+# clone repo
+if [ ! -d /tmp/gffread_source ]; then
+  git clone --depth 2 --single-branch --branch master https://github.com/gpertea/gffread.git /tmp/gffread_source
+fi
+
+# copy test data
+cp -r /tmp/gffread_source/examples/* src/gffread/test_data
