491d48edb4d14be5dcde2d246d7b411d36f80878
Nirmayi <ynirmayi@gmail.com>
2024-03-21 12:56:50 +0100

    check if output file has the right columns

--- src/salmon/quant/test.sh
+++ src/salmon/quant/test.sh
@@ -9,11 +9,15 @@ echo "> Run salmon quant for single-end reads"
   --lib_type "A" \
   --index "$dir_in/salmon/transcriptome_index" \
   --unmated_reads "$dir_in/reads/a_se.fq.gz" \
-  --output "quant_se_results"
+  --output "quant_se_results" \
+  --quant_results "quant_se.sf"
 
+cat quant_se.sf
 echo ">> Checking output"
-[ ! -d "quant_se_results" ] && echo "Output directory quant_se_results does not exist" && exit 
-[ ! -s "quant_se_results/quant.sf" ] && echo "Output file quant_se_results/quant.sf is empty" && exit 1
+[ ! -d "quant_se_results" ] && echo "Output directory quant_se_results does not exist" && exit 1
+[ ! -f "quant_se.sf" ] && echo "Output file quant_se.sf does not exist!" && exit 1
+[ ! -s "quant_se.sf" ] && echo "Output file quant_se.sf is empty!" && exit 1
+grep -q "Name	Length	EffectiveLength	TPM	NumReads" "quant_se.sf" || (echo "Output file quant_se.sf does not have the right format!" && exit 1)
 
 echo "> Run salmon quant for paired-end reads"
 "$meta_executable" \
@@ -21,11 +25,15 @@ echo "> Run salmon quant for paired-end reads"
   --index "$dir_in/salmon/transcriptome_index" \
   --mates1 "$dir_in/reads/a_1.fq.gz" \
   --mates2 "$dir_in/reads/a_2.fq.gz" \
-  --output "quant_pe_results"
+  --output "quant_pe_results" \
+  --quant_results "quant_pe.sf"
 
+cat quant_pe.sf
 echo ">> Checking output"
 [ ! -d "quant_pe_results" ] && echo "Output directory quant_pe_results does not exist" && exit 
-[ ! -s "quant_pe_results/quant.sf" ] && echo "Output file quant_pe_results/quant.sf is empty" && exit 1
+[ ! -f "quant_pe.sf" ] && echo "Output file quant_pe.sf does not exist!" && exit 1
+[ ! -s "quant_pe.sf" ] && echo "Output file quant_pe.sf is empty!" && exit 1
+grep -q "Name	Length	EffectiveLength	TPM	NumReads" "quant_pe.sf" || (echo "Output file quant_pe.sf does not have the right format!" && exit 1)
 
 echo "> Run salmon quant for paired-end reads with technical replicates"
 "$meta_executable" \
@@ -33,10 +41,14 @@ echo "> Run salmon quant for paired-end reads with technical replicates"
   --index "$dir_in/salmon/transcriptome_index" \
   --mates1 $dir_in/reads/a_1.fq.gz $dir_in/reads/b_1.fq.gz \
   --mates2 $dir_in/reads/a_2.fq.gz $dir_in/reads/b_2.fq.gz \
-  --output "quant_pe_rep_results"
+  --output "quant_pe_rep_results" \
+  --quant_results "quant_pe_rep.sf"
 
+cat quant_pe_rep.sf
 echo ">> Checking output"
 [ ! -d "quant_pe_rep_results" ] && echo "Output directory quant_pe_rep_results does not exist" && exit 
-[ ! -s "quant_pe_rep_results/quant.sf" ] && echo "Output file quant_pe_rep_results/quant.sf is empty" && exit 1
+[ ! -f "quant_pe_rep.sf" ] && echo "Output file quant_pe_rep.sf does not exist!" && exit 1
+[ ! -s "quant_pe_rep.sf" ] && echo "Output file quant_pe_rep.sf is empty!" && exit 1
+grep -q "Name	Length	EffectiveLength	TPM	NumReads" "quant_pe_rep.sf" || (echo "Output file quant_pe_rep.sf does not have the right format!" && exit 1)
 
 echo "> Test successful"
