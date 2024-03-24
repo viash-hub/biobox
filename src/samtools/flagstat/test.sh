ae3164eb34d743fcf664ff653b1a977b3cab306f
emmarousseau <emmarou1@icloud.com>
2024-03-24 11:01:58 +0100

    Add test from nf-core

--- src/samtools/flagstat/test.sh
+++ src/samtools/flagstat/test.sh
@@ -19,8 +19,27 @@ diff "$meta_resources_dir/test_data/a.flagstat" "$meta_resources_dir/test_data/a
 
 rm "$meta_resources_dir/test_data/a.flagstat"
 
-################################################################################
+############################################################################################
 
-echo 
+echo ">>> Testing $meta_functionality_name with singletons in the input"
+
+"$meta_executable" \
+  --bam "$meta_resources_dir/test_data/test.paired_end.sorted.bam" \
+  --bai "$meta_resources_dir/test_data/test.paired_end.sorted.bam.bai" \
+  --output "$meta_resources_dir/test_data/test.paired_end.sorted.flagstat"
+
+echo ">>> Checking whether output exists"
+[ ! -f "$meta_resources_dir/test_data/test.paired_end.sorted.flagstat" ] && echo "File 'test.paired_end.sorted.flagstat' does not exist!" && exit 1
+
+echo ">>> Checking whether output is non-empty"
+[ ! -s "$meta_resources_dir/test_data/test.paired_end.sorted.flagstat" ] && echo "File 'test.paired_end.sorted.flagstat' is empty!" && exit 1
+
+echo ">>> Checking whether output is correct"
+diff "$meta_resources_dir/test_data/test.paired_end.sorted.flagstat" "$meta_resources_dir/test_data/test_ref.paired_end.sorted.flagstat" || \
+    (echo "Output file chr19.flagstat does not match expected output" && exit 1)
+
+rm "$meta_resources_dir/test_data/test.paired_end.sorted.flagstat"
+
+
 echo "All tests succeeded!"
 exit 0
--- src/samtools/flagstat/test_data/script.sh
+++ src/samtools/flagstat/test_data/script.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 # Download test data from snakemake wrapper
+
 wget https://raw.githubusercontent.com/snakemake/snakemake-wrappers/3a4f7004281efc176fd9af732ad88d00c47d432d/bio/samtools/flagstat/test/mapped/a.bam
 samtools index a.bam
+# samtools flagstat a.bam > a_ref.flagstat
+
+
+# Download test data from nf-core module
+
+wget https://github.com/nf-core/test-datasets/raw/modules/data/genomics/sarscov2/illumina/bam/test.paired_end.sorted.bam
+wget https://github.com/nf-core/test-datasets/raw/modules/data/genomics/sarscov2/illumina/bam/test.paired_end.sorted.bam.bai
+# samtools flagstat test.paired_end.sorted.bam > test_ref.paired_end.sorted.flagstat
