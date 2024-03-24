9d909248e7041596b9b8a01bcff3f52880a2620d
emmarousseau <emmarou1@icloud.com>
2024-03-24 10:42:41 +0100

    Change to smaller test data

--- src/samtools/flagstat/test.sh
+++ src/samtools/flagstat/test.sh
@@ -3,21 +3,24 @@
 echo ">>> Testing $meta_functionality_name"
 
 "$meta_executable" \
-  --bam "$meta_resources_dir/test_data/chr19.bam" \
-  --bai "$meta_resources_dir/test_data/chr19.bam.bai" \
-  --output "$meta_resources_dir/test_data/chr19.flagstat"
+  --bam "$meta_resources_dir/test_data/a.bam" \
+  --bai "$meta_resources_dir/test_data/a.bam.bai" \
+  --output "$meta_resources_dir/test_data/a.flagstat"
 
 echo ">>> Checking whether output exists"
-[ ! -f "$meta_resources_dir/test_data/chr19.flagstat" ] && echo "File 'chr19.flagstat' does not exist!" && exit 1
+[ ! -f "$meta_resources_dir/test_data/a.flagstat" ] && echo "File 'a.flagstat' does not exist!" && exit 1
 
 echo ">>> Checking whether output is non-empty"
-[ ! -s "$meta_resources_dir/test_data/chr19.flagstat" ] && echo "File 'chr19.flagstat' is empty!" && exit 1
+[ ! -s "$meta_resources_dir/test_data/a.flagstat" ] && echo "File 'a.flagstat' is empty!" && exit 1
 
 echo ">>> Checking whether output is correct"
-diff "$meta_resources_dir/test_data/chr19.flagstat" "$meta_resources_dir/test_data/chr19_ref.flagstat" || \
+diff "$meta_resources_dir/test_data/a.flagstat" "$meta_resources_dir/test_data/a_ref.flagstat" || \
     (echo "Output file chr19.flagstat does not match expected output" && exit 1)
 
-rm "$meta_resources_dir/test_data/chr19.flagstat"
+rm "$meta_resources_dir/test_data/a.flagstat"
 
+################################################################################
+
+echo 
 echo "All tests succeeded!"
 exit 0
--- src/samtools/flagstat/test_data/script.sh
+++ src/samtools/flagstat/test_data/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Download test data
-wget https://github.com/CGATOxford/UMI-tools/raw/master/tests/chr19.bam
-wget https://github.com/CGATOxford/UMI-tools/raw/master/tests/chr19.bam.bai
-
+# Download test data from snakemake wrapper
+wget https://raw.githubusercontent.com/snakemake/snakemake-wrappers/3a4f7004281efc176fd9af732ad88d00c47d432d/bio/samtools/flagstat/test/mapped/a.bam
+samtools index a.bam
