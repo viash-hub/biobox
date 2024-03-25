6bc90ce76a8bca7bc61fc58623dcd82899a467fd
Nirmayi <ynirmayi@gmail.com>
2024-03-25 18:55:00 +0100

    check info.json output

--- src/salmon/salmon_index/test.sh
+++ src/salmon/salmon_index/test.sh
@@ -4,15 +4,17 @@ set -e
 
 dir_in="$meta_resources_dir/test_data"
 
-echo ">>> Run salmon_index"
+printf ">>> Run salmon_index"
 "$meta_executable" \
   --transcripts $dir_in/transcriptome.fasta \
-  --index index
+  --index index \
+  --kmer_len 31
 
-echo ">>> Checking whether output exists"
+printf ">>> Checking whether output exists"
 [ ! -d "index" ] && echo "'index' does not exist!" && exit 1
 [ -z "$(ls -A 'index')" ] && echo "'index' is empty!" && exit 1
 [ ! -f "index/info.json" ] && echo "Salmon index does not contain 'info.json'! Not all files were generated correctly!" && exit 1
+[ $(grep '"k": [0-9]*' index/info.json | cut -d':' -f2) != '31,' ] && printf "The generated Salmon index seems to be incorrect!" && exit 1
 
 echo "All tests succeeded!"
 exit 0
