f1cc6c0d65d8530d1ba293c0c7bdbed008bd8945
emmarousseau <emmarou1@icloud.com>
2024-04-07 16:25:27 +0100

    Add tests and reference outputs, update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -38,6 +38,7 @@
 * `samtools`:
     - `samtools/flagstat`: Counts the number of alignments in SAM/BAM/CRAM files for each FLAG type (PR #31).
     - `samtools/idxstats`: Reports alignment summary statistics for a SAM/BAM/CRAM file (PR #32).
+    - `samtools/samtools_stats`: Reports alignment summary statistics for a BAM file (PR #38).
 
 ## MAJOR CHANGES
 
---
+++ src/samtools/samtools_stats/test.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+test_dir="${meta_resources_dir}test_data"
+
+############################################################################################
+
+echo ">>> Test 1: $meta_functionality_name"
+"$meta_executable" \
+  --input "$test_dir/test.paired_end.sorted.bam" \
+  --bai "$test_dir/test.paired_end.sorted.bam.bai" \
+  --output "$test_dir/test.paired_end.sorted.txt"
+
+echo ">>> Checking whether output exists"
+[ ! -f "$test_dir/test.paired_end.sorted.txt" ] && echo "File 'test.paired_end.sorted.txt' does not exist!" && exit 1
+
+echo ">>> Checking whether output is non-empty"
+[ ! -s "$test_dir/test.paired_end.sorted.txt" ] && echo "File 'test.paired_end.sorted.txt' is empty!" && exit 1
+
+echo ">>> Checking whether output is correct"
+# compare using diff, but ingnoring the header line (starting with "#")
+diff <(grep -v "^#" "$test_dir/test.paired_end.sorted.txt") \
+    <(grep -v "^#" "$test_dir/ref.paired_end.sorted.txt") || \
+    (echo "Output file ref.paired_end.sorted.txt does not match expected output" && exit 1)
+    
+rm "$test_dir/test.paired_end.sorted.txt"
+
+############################################################################################
+
+echo ">>> Test 2: $meta_functionality_name with --remove_dups"
+"$meta_executable" \
+  --remove_dups \
+  --input "$test_dir/test.paired_end.sorted.bam" \
+  --bai "$test_dir/test.paired_end.sorted.bam.bai" \
+  --output "$test_dir/test.d.paired_end.sorted.txt"
+
+echo ">>> Checking whether output exists"
+[ ! -f "$test_dir/ref.d.paired_end.sorted.txt" ] && echo "File 'ref.d.paired_end.sorted.txt' does not exist!" && exit 1
+
+echo ">>> Checking whether output is non-empty"
+[ ! -s "$test_dir/ref.d.paired_end.sorted.txt" ] && echo "File 'ref.d.paired_end.sorted.txt' is empty!" && exit 1
+
+echo ">>> Checking whether output is correct"
+# compare using diff, but ingnoring the header line (starting with "#")
+diff <(grep -v "^#" "$test_dir/test.d.paired_end.sorted.txt") \
+    <(grep -v "^#" "$test_dir/ref.d.paired_end.sorted.txt") || \
+    (echo "Output file ref.d.paired_end.sorted.txt does not match expected output" && exit 1)
+
+rm "$test_dir/test.d.paired_end.sorted.txt"
+
+############################################################################################
+
+echo ">>> Test 3: $meta_functionality_name with --remove_overlaps"
+"$meta_executable" \
+  --remove_overlaps \
+  --input "$test_dir/test.paired_end.sorted.bam" \
+  --bai "$test_dir/test.paired_end.sorted.bam.bai" \
+  --output "$test_dir/test.p.paired_end.sorted.txt" 
+
+echo ">>> Checking whether output exists"
+[ ! -f "$test_dir/ref.p.paired_end.sorted.txt" ] && echo "File 'ref.p.paired_end.sorted.txt' does not exist!" && exit 1
+
+echo ">>> Checking whether output is non-empty"
+[ ! -s "$test_dir/ref.p.paired_end.sorted.txt" ] && echo "File 'ref.p.paired_end.sorted.txt' is empty!" && exit 1
+
+
+echo ">>> Checking whether output is correct"
+# compare using diff, but ingnoring the header line (starting with "#")
+diff <(grep -v "^#" "$test_dir/test.p.paired_end.sorted.txt") \
+    <(grep -v "^#" "$test_dir/ref.p.paired_end.sorted.txt") || \
+    (echo "Output file ref.p.paired_end.sorted.txt does not match expected output" && exit 1)
+
+rm "$test_dir/test.p.paired_end.sorted.txt"
+
+############################################################################################
+
+echo ">>> All tests passed successfully."
+
+exit 0
