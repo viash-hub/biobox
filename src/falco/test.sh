03f66385a48013c60097cb23d0adbd7af9161a39
Toni Verbeiren <toni.verbeiren@gmail.com>
2024-04-29 21:14:12 +0200

    Apply suggestions from code review
    Co-authored-by: Dries Schaumont <5946712+DriesSchaumont@users.noreply.github.com>

--- src/falco/test.sh
+++ src/falco/test.sh
@@ -24,56 +24,56 @@ echo ">> Run falco on test data, output to dir"
 echo ">>> Run falco"
 $meta_executable \
   --input "$test_data_dir/R1.fastq.gz;$test_data_dir/R2.fastq.gz" \
-  --outdir out/
+  --outdir "$TMPDIR/output1"
 
 echo ">>> Checking whether output exists"
-[ ! -d "out" ] && echo "Output directory not created" && exit 1
-[ ! -f "out/R1.fastq.gz_fastqc_report.html" ] && echo "Report not created" && exit 1
-[ ! -f "out/R1.fastq.gz_summary.txt" ] && echo "Summary not created" && exit 1
-[ ! -f "out/R1.fastq.gz_fastqc_data.txt" ] && echo "fastqc_data not created" && exit 1
-[ ! -f "out/R2.fastq.gz_fastqc_report.html" ] && echo "Report not created" && exit 1
-[ ! -f "out/R2.fastq.gz_summary.txt" ] && echo "Summary not created" && exit 1
-[ ! -f "out/R2.fastq.gz_fastqc_data.txt" ] && echo "fastqc_data not created" && exit 1
+[ ! -d "$TMPDIR/output1" ] && echo "Output directory not created" && exit 1
+[ ! -f "$TMPDIR/output1/R1.fastq.gz_fastqc_report.html" ] && echo "Report not created" && exit 1
+[ ! -f "$TMPDIR/output1/R1.fastq.gz_summary.txt" ] && echo "Summary not created" && exit 1
+[ ! -f "$TMPDIR/output1/R1.fastq.gz_fastqc_data.txt" ] && echo "fastqc_data not created" && exit 1
+[ ! -f "$TMPDIR/output1/R2.fastq.gz_fastqc_report.html" ] && echo "Report not created" && exit 1
+[ ! -f "$TMPDIR/output1/R2.fastq.gz_summary.txt" ] && echo "Summary not created" && exit 1
+[ ! -f "$TMPDIR/output1/R2.fastq.gz_fastqc_data.txt" ] && echo "fastqc_data not created" && exit 1
 
 echo ">>> cleanup"
-rm -rf out/
+rm -rf "$TMPDIR/output1"
 
 echo ">> Run falco on test data, output to individual files"
 echo ">>> Please note this is only possible for 1 input fastq file!"
 echo ">>> Run falco"
 $meta_executable \
   --input "$test_data_dir/R1.fastq.gz" \
-  --data_filename out/data.txt \
-  --report_filename out/report.html \
-  --summary_filename out/summary.txt \
-  --outdir out/
+  --data_filename "$TMPDIR/output2/data.txt" \
+  --report_filename "$TMPDIR/output2/report.html" \
+  --summary_filename "$TMPDIR/output2/summary.txt" \
+  --outdir "$TMPDIR/output2/"
 
 echo ">>> Checking whether output exists"
-[ ! -d "out" ] && echo "Output directory not created" && exit 1
-[ ! -f "out/report.html" ] && echo "Report not created" && exit 1
-[ ! -f "out/summary.txt" ] && echo "Summary not created" && exit 1
-[ ! -f "out/data.txt" ] && echo "fastqc_data not created" && exit 1
+[ ! -d "$TMPDIR/output2" ] && echo "Output directory not created" && exit 1
+[ ! -f "$TMPDIR/output2/report.html" ] && echo "Report not created" && exit 1
+[ ! -f "$TMPDIR/output2/summary.txt" ] && echo "Summary not created" && exit 1
+[ ! -f "$TMPDIR/output2/data.txt" ] && echo "fastqc_data not created" && exit 1
 
 echo ">>> cleanup"
-rm -rf out/
+rm -rf $TMPDIR/output2/
 
 echo ">> Run falco on test data, subsample"
 echo ">>> Run falco"
 $meta_executable \
   --input "$test_data_dir/R1.fastq.gz" \
-  --data_filename out/data.txt \
-  --report_filename out/report.html \
-  --summary_filename out/summary.txt \
+  --data_filename "$TMPDIR/output3/data.txt" \
+  --report_filename "$TMPDIR/output3/report.html" \
+  --summary_filename "$TMPDIR/output3/summary.txt" \
   --subsample 100 \
-  --outdir out
+  --outdir "$TMPDIR/output3"
 
 echo ">>> Checking whether output exists"
-[ ! -d "out" ] && echo "Output directory not created" && exit 1
-[ ! -f "out/report.html" ] && echo "Report not created" && exit 1
-[ ! -f "out/summary.txt" ] && echo "Summary not created" && exit 1
-[ ! -f "out/data.txt" ] && echo "fastqc_data not created" && exit 1
+[ ! -d "$TMPDIR/output3" ] && echo "Output directory not created" && exit 1
+[ ! -f "$TMPDIR/output3/report.html" ] && echo "Report not created" && exit 1
+[ ! -f "$TMPDIR/output3/summary.txt" ] && echo "Summary not created" && exit 1
+[ ! -f "$TMPDIR/output3/data.txt" ] && echo "fastqc_data not created" && exit 1
 
 echo ">>> cleanup"
-rm -rf out/
+rm -rf "$TMPDIR/output3/"
 
 echo "All tests succeeded!"
