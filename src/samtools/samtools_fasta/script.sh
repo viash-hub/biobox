2d939443adfcce61ab59e01e14c1b1435e50068a
emmarousseau <emmarou1@icloud.com>
2024-05-23 15:40:39 +0200

    Fasta component

---
+++ src/samtools/samtools_fasta/script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+set -e
+
+[[ "$par_no_suffix" == "false" ]] && unset par_no_suffix
+[[ "$par_suffix" == "false" ]] && unset par_suffix
+[[ "$par_use_oq" == "false" ]] && unset par_use_oq
+[[ "$par_copy_tags" == "false" ]] && unset par_copy_tags
+[[ "$par_casava" == "false" ]] && unset par_casava
+
+samtools fasta \
+    ${par_no_suffix:+-n} \
+    ${par_suffix:+-N} \
+    ${par_use_oq:+-O} \
+    ${par_singleton:+-s "$par_singleton"} \
+    ${par_copy_tags:+-t} \
+    ${par_copy_tags_list:+-T "$par_copy_tags_list"} \
+    ${par_read1:+-1 "$par_read1"} \
+    ${par_read2:+-2 "$par_read2"} \
+    ${par_output_reads:+-o "$par_output_reads"} \
+    ${par_output_reads_both:+-0 "$par_output_reads_both"} \
+    ${par_filter_flags:+-f "$par_filter_flags"} \
+    ${par_excl_flags:+-F "$par_excl_flags"} \
+    ${par_incl_flags:+--rf "$par_incl_flags"} \
+    ${par_excl_flags_all:+-G "$par_excl_flags_all"} \
+    ${par_aux_tag:+-d "$par_aux_tag"} \
+    ${par_aux_tag_file:+-D "$par_aux_tag_file"} \
+    ${par_casava:+-i} \
+    ${par_compression:+-c "$par_compression"} \
+    ${par_index1:+--i1 "$par_index1"} \
+    ${par_index2:+--i2 "$par_index2"} \
+    ${par_barcode_tag:+--barcode-tag "$par_barcode_tag"} \
+    ${par_quality_tag:+--quality-tag "$par_quality_tag"} \
+    ${par_index_format:+--index-format "$par_index_format"} \
+    "$par_input" \
+    > "$par_output"
+
