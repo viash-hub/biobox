5072b3e57e760ed42da2c4f62b3c62ce4d8d31d5
emmarousseau <emmarou1@icloud.com>
2024-06-02 20:00:54 +0200

    include arguments and script from the rnaseq.vsh version

--- src/umi_tools/umi_tools_extract/script.sh
+++ src/umi_tools/umi_tools_extract/script.sh
@@ -3,11 +3,11 @@
 ## VIASH START
 ## VIASH END
 
-set -e
+set -eo pipefail
 
 test_dir="${metal_executable}/test_data"
 
-
+[[ "$par_paired" == "false" ]] && unset par_paired
 [[ "$par_error_correct_cell" == "false" ]] && unset par_error_correct_cell
 [[ "$par_reconcile_pairs" == "false" ]] && unset par_reconcile_pairs
 [[ "$par_3prime" == "false" ]] && unset par_3prime
@@ -15,36 +15,108 @@ test_dir="${metal_executable}/test_data"
 [[ "$par_timeit_header" == "false" ]] && unset par_timeit_header
 [[ "$par_log2stderr" == "false" ]] && unset par_log2stderr
 
-umi_tools extract \
-    --stdin "$par_input" \
-    --stdout "$par_output" \
-    ${par_output_stats:+--output-stats "$par_output_stats"} \
-    ${par_error_correct_cell:+--error-correct-cell} \
-    ${par_whitelist:+--whitelist "$par_whitelist"} \
-    ${par_blacklist:+--blacklist "$par_blacklist"} \
-    ${par_subset_reads:+--subset-reads "$par_subset_reads"} \
-    ${par_quality_filter_threshold:+--quality-filter-threshold "$par_quality_filter_threshold"} \
-    ${par_quality_filter_mask:+--quality-filter-mask "$par_quality_filter_mask"} \
-    ${par_quality_encoding:+--quality-encoding "$par_quality_encoding"} \
-    ${par_reconcile_pairs:+--reconcile-pairs} \
-    ${par_bc_pattern:+--bc-pattern "$par_bc_pattern"} \
-    ${par_bc_pattern2:+--bc-pattern2 "$par_bc_pattern2"} \
-    ${par_extract_method:+--extract-method "$par_extract_method"} \
-    ${par_three_prime:+--3prime} \
-    ${par_read2_in:+--read2-in "$par_read2_in"} \
-    ${par_filtered_out:+--filtered-out "$par_filtered_out"} \
-    ${par_filtered_out2:+--filtered-out2 "$par_filtered_out2"} \
-    ${par_ignore_read_pair_suffixes:+--ignore-read-pair-suffixes} \
-    ${par_random_seed:+--random-seed "$par_random_seed"} \
-    ${par_temp_dir:+--temp-dir "$par_temp_dir"} \
-    ${par_compresslevel:+--compresslevel "$par_compresslevel"} \
-    ${par_timeit:+--timeit "$par_timeit"} \
-    ${par_timeit_name:+--timeit-name "$par_timeit_name"} \
-    ${par_timeit_header:+--timeit-header} \
-    ${par_log:+--log "$par_log"} \
-    ${par_log2stderr:+--log2stderr} \
-    ${par_verbose:+--verbose "$par_verbose"} \
-    ${par_error:+--error "$par_error"}
 
+function clean_up {
+    rm -rf "$tmpdir"
+}
+trap clean_up EXIT 
 
-exit 0
+tmpdir=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXXXX")
+
+IFS="," read -ra input <<< "$par_input"
+IFS="," read -ra pattern <<< "$par_bc_pattern"
+
+read_count="${#input[@]}"
+pattern_count="${#pattern[@]}"
+
+if [ "$par_paired" == "true" ]; then
+    echo "Paired - Reads: $read_count bc_patterns: $pattern_count"
+    if [ "$read_count" -ne 2 ] || [ "$pattern_count" -ne 2 ]; then
+        echo "Paired end input requires two read files and two UMI patterns"
+        exit 1
+    else
+        read1="$(basename -- ${input[0]})"
+        read2="$(basename -- ${input[1]})"
+        umi_tools extract \
+            -I "${input[0]}" \
+            --read2-in "${input[1]}" \
+            -S "$tmpdir/$read1" \
+            --read2-out="$tmpdir/$read2" \
+            ${par_umitools_extract_method:+--extract-method "$par_umitools_extract_method"} \
+            --bc-pattern "${pattern[0]}" \
+            --bc-pattern2 "${pattern[1]}" \
+            ${par_umitools_umi_separator:+--umi-separator "$par_umitools_umi_separator"} \
+            ${par_output_stats:+--output-stats "$par_output_stats"} \
+            ${par_error_correct_cell:+--error-correct-cell} \
+            ${par_whitelist:+--whitelist "$par_whitelist"} \
+            ${par_blacklist:+--blacklist "$par_blacklist"} \
+            ${par_subset_reads:+--subset-reads "$par_subset_reads"} \
+            ${par_quality_filter_threshold:+--quality-filter-threshold "$par_quality_filter_threshold"} \
+            ${par_quality_filter_mask:+--quality-filter-mask "$par_quality_filter_mask"} \
+            ${par_quality_encoding:+--quality-encoding "$par_quality_encoding"} \
+            ${par_reconcile_pairs:+--reconcile-pairs} \
+            ${par_three_prime:+--3prime} \
+            ${par_filtered_out:+--filtered-out "$par_filtered_out"} \
+            ${par_filtered_out2:+--filtered-out2 "$par_filtered_out2"} \
+            ${par_ignore_read_pair_suffixes:+--ignore-read-pair-suffixes} \
+            ${par_random_seed:+--random-seed "$par_random_seed"} \
+            ${par_temp_dir:+--temp-dir "$par_temp_dir"} \
+            ${par_compresslevel:+--compresslevel "$par_compresslevel"} \
+            ${par_timeit:+--timeit "$par_timeit"} \
+            ${par_timeit_name:+--timeit-name "$par_timeit_name"} \
+            ${par_timeit_header:+--timeit-header} \
+            ${par_log:+--log "$par_log"} \
+            ${par_log2stderr:+--log2stderr} \
+            ${par_verbose:+--verbose "$par_verbose"} \
+            ${par_error:+--error "$par_error"}
+        if [ $par_umi_discard_read == 1 ]; then
+            # discard read 1
+            cp $tmpdir/$read2 $par_fastq_2
+        elif [ $par_umi_discard_read == 2 ]; then
+            # discard read 2
+            cp $tmpdir/$read1 $par_fastq_1
+        else
+            cp $tmpdir/$read1 $par_fastq_1
+            cp $tmpdir/$read2 $par_fastq_2
+        fi
+    fi
+else
+    echo "Not Paired - $read_count"
+    if [ "$read_count" -ne 1 ] || [ "$pattern_count" -ne 1 ]; then
+        echo "Single end input requires one read file and one UMI pattern"
+        exit 1
+    else
+        read1="$(basename -- ${input[0]})"
+        umi_tools extract \
+            -I "${input[0]}" \
+            -S "$tmpdir/$read1" \
+            --bc-pattern "${pattern[0]}" \
+            ${par_umitools_extract_method:+--extract-method "$par_umitools_extract_method"} \
+            ${par_umitools_umi_separator:+--umi-separator "$par_umitools_umi_separator"} \
+            ${par_output_stats:+--output-stats "$par_output_stats"} \
+            ${par_error_correct_cell:+--error-correct-cell} \
+            ${par_whitelist:+--whitelist "$par_whitelist"} \
+            ${par_blacklist:+--blacklist "$par_blacklist"} \
+            ${par_subset_reads:+--subset-reads "$par_subset_reads"} \
+            ${par_quality_filter_threshold:+--quality-filter-threshold "$par_quality_filter_threshold"} \
+            ${par_quality_filter_mask:+--quality-filter-mask "$par_quality_filter_mask"} \
+            ${par_quality_encoding:+--quality-encoding "$par_quality_encoding"} \
+            ${par_reconcile_pairs:+--reconcile-pairs} \
+            ${par_three_prime:+--3prime} \
+            ${par_filtered_out:+--filtered-out "$par_filtered_out"} \
+            ${par_filtered_out2:+--filtered-out2 "$par_filtered_out2"} \
+            ${par_ignore_read_pair_suffixes:+--ignore-read-pair-suffixes} \
+            ${par_random_seed:+--random-seed "$par_random_seed"} \
+            ${par_temp_dir:+--temp-dir "$par_temp_dir"} \
+            ${par_compresslevel:+--compresslevel "$par_compresslevel"} \
+            ${par_timeit:+--timeit "$par_timeit"} \
+            ${par_timeit_name:+--timeit-name "$par_timeit_name"} \
+            ${par_timeit_header:+--timeit-header} \
+            ${par_log:+--log "$par_log"} \
+            ${par_log2stderr:+--log2stderr} \
+            ${par_verbose:+--verbose "$par_verbose"} \
+            ${par_error:+--error "$par_error"}
+
+        cp $tmpdir/$read1 $par_fastq_1
+    fi
+fi
--- src/umi_tools/umi_tools_extract/test.sh
+++ src/umi_tools/umi_tools_extract/test.sh
@@ -1,55 +1,81 @@
 #!/bin/bash
 
 test_dir="${meta_resources_dir}/test_data"
-out_dir="${meta_resources_dir}/out"
 
-mkdir -p "$out_dir"
+echo ">>> Testing $meta_functionality_name"
 
-#############################################################################################
+############################################################################################################
 
 echo ">>> Test 1: Testing for paired-end reads"
-
 "$meta_executable" \
-    --input "$test_dir/scrb_seq_fastq.1.gz" \
-    --read2_in "$test_dir/scrb_seq_fastq.2.gz" \
-    --bc_pattern CCCCCCNNNNNNNNNN \
-    --bc_pattern2 CCCCCCNNNNNNNNNN \
-    --extract_method string \
-    --output "$out_dir/scrb_seq_fastq.1.umi_extract.fastq"
+    --paired \
+    --input "$test_dir/scrb_seq_fastq.1.gz","$test_dir/scrb_seq_fastq.2.gz" \
+    --bc_pattern "CCCCCCNNNNNNNNNN","CCCCCCNNNNNNNNNN" \
+    --umitools_extract_method string \
+    --umitools_umi_separator '_' \
+    --umitools_grouping_method directional \
+    --umi_discard_read 0 \
+    --fastq_1 scrb_seq_fastq.1.umi_extract.fastq.gz \
+    --fastq_2 scrb_seq_fastq.2.umi_extract.fastq.gz \
+    --log log.out \
+    --random_seed 1
 
 echo ">> Checking if the correct files are present"
-[[ ! -f "$out_dir/scrb_seq_fastq.1.umi_extract.fastq" ]] && echo "Reads file missing" && exit 1
-[ ! -s "$out_dir/scrb_seq_fastq.1.umi_extract.fastq" ] && echo "Read 1 file is empty" && exit 1
+[[ ! -f "scrb_seq_fastq.1.umi_extract.fastq.gz" ]] || [[ ! -f "scrb_seq_fastq.2.umi_extract.fastq.gz" ]] && echo "Reads file missing" && exit 1
+[ ! -s "scrb_seq_fastq.1.umi_extract.fastq.gz" ] && echo "Read 1 file is empty" && exit 1
+[ ! -s "scrb_seq_fastq.2.umi_extract.fastq.gz" ] && echo "Read 2 file is empty" && exit 1
 
-rm "$out_dir/scrb_seq_fastq.1.umi_extract.fastq"
+gunzip -c scrb_seq_fastq.1.umi_extract.fastq.gz > scrb_seq_fastq.1.umi_extract.fastq
+cat scrb_seq_fastq.1.umi_extract.fastq
 
-#############################################################################################
+echo ">> Checking if the files are correct"
+diff -q <(gunzip -c "${meta_resources_dir}/scrb_seq_fastq.1.umi_extract.fastq.gz") <(gunzip -c "$test_dir/scrb_seq_fastq.1.umi_extract.fastq.gz") || \
+    (echo "Read 1 file is not correct" && exit 1)
+diff -q <(gunzip -c "${meta_resources_dir}/scrb_seq_fastq.2.umi_extract.fastq.gz"z) <(gunzip -c "$test_dir/scrb_seq_fastq.2.umi_extract.fastq.gz") || \
+    (echo "Read 2 file is not correct" && exit 1)
+
+rm scrb_seq_fastq.1.umi_extract.fastq.gz scrb_seq_fastq.2.umi_extract.fastq.gz
+
+############################################################################################################
 
 echo ">>> Test 2: Testing for paired-end reads with umi_discard_reads option"
 "$meta_executable" \
-    --input "$test_dir/scrb_seq_fastq.1.gz" \
-    --read2_in "$test_dir/scrb_seq_fastq.2.gz" \
-    --bc_pattern CCCCCCNNNNNNNNNN \
-    --bc_pattern2 CCCCCCNNNNNNNNNN \
-    --extract_method string \
-    --output "$out_dir/scrb_seq_fastq.1.umi_extract.fastq"
+    --paired \
+    --input "$test_dir/scrb_seq_fastq.1.gz","$test_dir/scrb_seq_fastq.2.gz" \
+    --bc_pattern CCCCCCNNNNNNNNNN,CCCCCCNNNNNNNNNN \
+    --umitools_extract_method string \
+    --umitools_umi_separator '_' \
+    --umitools_grouping_method directional \
+    --umi_discard_read 2 \
+    --fastq_1 scrb_seq_fastq.1.umi_extract.fastq.gz \
+    --log log.out \
+    --random_seed 1
 
 echo ">> Checking if the correct files are present"
-[ ! -f "$out_dir/scrb_seq_fastq.1.umi_extract.fastq" ] && echo "Read 1 file is missing" && exit 1
-[ ! -s "$out_dir/scrb_seq_fastq.1.umi_extract.fastq" ] && echo "Read 1 file is empty" && exit 1
+[ ! -f "scrb_seq_fastq.1.umi_extract.fastq.gz" ] && echo "Read 1 file is missing" && exit 1
+[ ! -s "scrb_seq_fastq.1.umi_extract.fastq.gz" ] && echo "Read 1 file is empty" && exit 1
+[ -f "scrb_seq_fastq.2.umi_extract.fastq.gz" ] && echo "Read 2 is not discarded" && exit 1
 
-#############################################################################################
+############################################################################################################
 
 echo ">>> Test 3: Testing for single-end reads"
 "$meta_executable" \
     --input "$test_dir/slim.fastq.gz" \
     --bc_pattern  "^(?P<umi_1>.{3}).{4}(?P<umi_2>.{2})" \
-    --extract_method regex \
-    --output "$out_dir/slim.umi_extract.fastq"
+    --umitools_extract_method regex \
+    --umitools_umi_separator '_' \
+    --umitools_grouping_method directional \
+    --umi_discard_read 0 \
+    --fastq_1 slim.umi_extract.fastq.gz \
+    --log log.out \
+    --random_seed 1 
 
 echo ">> Checking if the correct files are present"
-[ ! -f "$out_dir/slim.umi_extract.fastq" ] && echo "Trimmed reads file missing" && exit 1
-[ ! -s "$out_dir/slim.umi_extract.fastq" ] && echo "Trimmed reads file is empty" && exit 1
+[ ! -f "slim.umi_extract.fastq.gz" ] && echo "Trimmed reads file missing" && exit 1
+[ ! -s "slim.umi_extract.fastq.gz" ] && echo "Trimmed reads file is empty" && exit 1
+
+echo ">>> Test finished successfully"
+exit 0
 
 
 echo ">>> Test finished successfully"
