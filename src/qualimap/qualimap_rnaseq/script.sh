e9dbe7c4a7d881d2dca506839fd44d865fe3f675
dorien-er <roosen.dorien@gmail.com>
2024-06-07 16:42:14 +0200

    complete script for qualimap

--- src/qualimap/qualimap_rnaseq/script.sh
+++ src/qualimap/qualimap_rnaseq/script.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
-sleep 10000
-
 set -eo pipefail
 
-mkdir -p $par_output_dir
+tmp_dir=$(mktemp -d -p "$meta_temp_dir" qualimap_XXXXXXXXX)
+
+
+if [ -n "$par_output_file" ]; then
+    outfile=$(basename "$par_output_file")
+fi
+
+if [ -n "$par_output_counts" ]; then
+    counts=$(basename "$par_output_counts")
+fi
 
 qualimap rnaseq \
-    # --algorithm=$par_algorithm \
     --java-mem-size=$par_java_memory_size \
+    --algorithm $par_algorithm \
     --num-pr-bases $par_pr_bases \
     --num-tr-bias $par_tr_bias \
     --sequencing-protocol $par_sequencing_protocol \
     -bam $par_input \
     -gtf $par_gtf \
-    ${par_paired:+-pe} \
-    ${par_sorted:+-s} \
-    -outdir $par_output_dir \
-    # -oc $par_output_counts \
-    # -outfile $par_output_file \
-    -outformat $par_output_format 
+    -outdir "$tmp_dir" \
+    -outformat pdf \
+    ${par_output_file:+-outfile "$outfile"} \
+    ${par_output_counts:+-oc "$counts"}
+
+mv "$tmp_dir/rnaseq_qc_results.txt" "$par_output"
+
+if [ -n "$par_output_file" ]; then
+    mv "$tmp_dir/$outfile" "$par_output_file"
+fi
+
+if [ -n "$par_output_counts" ]; then
+    mv "$tmp_dir/$counts" "$par_output_counts"
+fi
