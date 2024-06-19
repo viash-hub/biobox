7b3e329ab15ddaabc5dfe59faeb9ab2877a09ae6
Leila011 <leilapaquay@gmail.com>
2024-06-19 10:18:43 +0200

    add test data

---
+++ src/agat/agat_convertspgff2gtf/test_data/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# clone repo
+if [ ! -d /tmp/agat_source ]; then
+  git clone --depth 1 --single-branch --branch master https://github.com/NBISweden/AGAT /tmp/agat_source
+fi
+
+# copy test data
+cp -r /tmp/agat_source/t/gff_syntax/in/0_test.gff src/agat/agat_convertspgff2gtf/test_data
+
+
+
