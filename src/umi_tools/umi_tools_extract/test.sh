ec951d831e3bb3c8df57910c85ee08dcc4e0e0fc
emmarousseau <emmarou1@icloud.com>
2024-06-28 18:18:05 +0100

    new version of the script

--- src/umi_tools/umi_tools_extract/test.sh
+++ src/umi_tools/umi_tools_extract/test.sh
@@ -8,15 +8,17 @@ echo ">>> Testing $meta_functionality_name"
 
 echo ">>> Test 1: Testing for paired-end reads"
 "$meta_executable" \
-    --paired \
-    --input "$test_dir/scrb_seq_fastq.1.gz","$test_dir/scrb_seq_fastq.2.gz" \
-    --bc_pattern "CCCCCCNNNNNNNNNN","CCCCCCNNNNNNNNNN" \
+    --paired true \
+    --input "$test_dir/scrb_seq_fastq.1.gz"\
+    --read2_in "$test_dir/scrb_seq_fastq.2.gz" \
+    --bc_pattern "CCCCCCNNNNNNNNNN"\
+    --bc_pattern2 "CCCCCCNNNNNNNNNN" \
     --umitools_extract_method string \
     --umitools_umi_separator '_' \
     --umitools_grouping_method directional \
     --umi_discard_read 0 \
-    --fastq_1 scrb_seq_fastq.1.umi_extract.fastq.gz \
-    --fastq_2 scrb_seq_fastq.2.umi_extract.fastq.gz \
+    --read1_out scrb_seq_fastq.1.umi_extract.fastq.gz \
+    --read2_out scrb_seq_fastq.2.umi_extract.fastq.gz \
     --log log.out \
     --random_seed 1
 
@@ -31,7 +33,7 @@ cat scrb_seq_fastq.1.umi_extract.fastq
 echo ">> Checking if the files are correct"
 diff -q <(gunzip -c "${meta_resources_dir}/scrb_seq_fastq.1.umi_extract.fastq.gz") <(gunzip -c "$test_dir/scrb_seq_fastq.1.umi_extract.fastq.gz") || \
     (echo "Read 1 file is not correct" && exit 1)
-diff -q <(gunzip -c "${meta_resources_dir}/scrb_seq_fastq.2.umi_extract.fastq.gz"z) <(gunzip -c "$test_dir/scrb_seq_fastq.2.umi_extract.fastq.gz") || \
+diff -q <(gunzip -c "${meta_resources_dir}/scrb_seq_fastq.2.umi_extract.fastq.gz") <(gunzip -c "$test_dir/scrb_seq_fastq.2.umi_extract.fastq.gz") || \
     (echo "Read 2 file is not correct" && exit 1)
 
 rm scrb_seq_fastq.1.umi_extract.fastq.gz scrb_seq_fastq.2.umi_extract.fastq.gz
@@ -40,14 +42,16 @@ rm scrb_seq_fastq.1.umi_extract.fastq.gz scrb_seq_fastq.2.umi_extract.fastq.gz
 
 echo ">>> Test 2: Testing for paired-end reads with umi_discard_reads option"
 "$meta_executable" \
-    --paired \
-    --input "$test_dir/scrb_seq_fastq.1.gz","$test_dir/scrb_seq_fastq.2.gz" \
-    --bc_pattern CCCCCCNNNNNNNNNN,CCCCCCNNNNNNNNNN \
+    --paired true \
+    --input "$test_dir/scrb_seq_fastq.1.gz" \
+    --read2_in "$test_dir/scrb_seq_fastq.2.gz" \
+    --bc_pattern CCCCCCNNNNNNNNNN \
+    --bc_pattern2 CCCCCCNNNNNNNNNN \
     --umitools_extract_method string \
     --umitools_umi_separator '_' \
     --umitools_grouping_method directional \
     --umi_discard_read 2 \
-    --fastq_1 scrb_seq_fastq.1.umi_extract.fastq.gz \
+    --read1_out scrb_seq_fastq.1.umi_extract.fastq.gz \
     --log log.out \
     --random_seed 1
 
@@ -60,23 +64,20 @@ echo ">> Checking if the correct files are present"
 
 echo ">>> Test 3: Testing for single-end reads"
 "$meta_executable" \
+    --paired false \
     --input "$test_dir/slim.fastq.gz" \
     --bc_pattern  "^(?P<umi_1>.{3}).{4}(?P<umi_2>.{2})" \
     --umitools_extract_method regex \
     --umitools_umi_separator '_' \
     --umitools_grouping_method directional \
     --umi_discard_read 0 \
-    --fastq_1 slim.umi_extract.fastq.gz \
+    --read1_out slim.umi_extract.fastq.gz \
     --log log.out \
     --random_seed 1 
 
 echo ">> Checking if the correct files are present"
 [ ! -f "slim.umi_extract.fastq.gz" ] && echo "Trimmed reads file missing" && exit 1
 [ ! -s "slim.umi_extract.fastq.gz" ] && echo "Trimmed reads file is empty" && exit 1
-
-echo ">>> Test finished successfully"
-exit 0
-
 
 echo ">>> Test finished successfully"
 exit 0
