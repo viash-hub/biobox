e7181c24cec51277c8ff51f049edc8c1f6fd5cbf
Leila011 <leilapaquay@gmail.com>
2024-06-28 16:10:44 +0200

    Create test script

---
+++ src/agat/agat_convertspgff2gtf/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+test_dir="${meta_resources_dir}/test_data"
+
+
+echo "> Run agat_convertspgff2gtf with test data"
+"$meta_executable" \
+  --gff "$test_dir/0_test.gff" \
+  --output "output.gtf" 
+
+echo ">> Checking output"
+[ ! -f "output.gtf" ] && echo "Output file output.gtf does not exist" && exit 1
+
+echo ">> Check if output is empty"
+[ ! -s "output.gtf" ] && echo "Output file output.gtf is empty" && exit 1
+
+rm output.gtf
+
+echo "> Test successful"
