9ca70717c6094ad23d93a4abaa39700acb837b42
tgaspe <theodorogtc@gmail.com>
2024-07-15 22:56:42 -0300

    Update on test.sh

--- src/seqtk/seqtk_subseq/test.sh
+++ src/seqtk/seqtk_subseq/test.sh
@@ -31,6 +31,43 @@ if [ "$num_samples" -ne 2 ]; then
     exit 1
 fi
 
+# echo ">> Compare reads"
+# # Extract headers
+# headers1=$(grep '^@' sampled_1.fastq | sed -e's/ 1$//' | sort)
+# headers2=$(grep '^@' sampled_2.fastq | sed -e 's/ 2$//' | sort)
+
+# # Compare headers
+# diff <(echo "$headers1") <(echo "$headers2") || { echo "Mismatch detected"; exit 1; }
+
 #########################################################################################
 # ... add more tests here ...
+# test fq file
 
+# TODO: Figure out how the test fq file should look like and how the reg.bed file should look like
+
+#########################################################################################
+# test tab option
+echo "> Run seqtk_subseq with TAB option"
+"$meta_executable" \
+  --tab \
+  --input "$meta_resources_dir/test_data/input.fa" \
+  --name_list "$meta_resources_dir/test_data/list.lst" \
+  --output "sub_sampled.fa"
+
+#########################################################################################
+# test strand aware option
+echo "> Run seqtk_subseq with Strand Aware option"
+"$meta_executable" \
+  --strand_aware \
+  --input "$meta_resources_dir/test_data/input.fa" \
+  --name_list "$meta_resources_dir/test_data/list.lst" \
+  --output "sub_sampled.fa"
+
+#########################################################################################
+# test sequence line length option
+echo "> Run seqtk_subseq with line length option"
+"$meta_executable" \
+  --sequence_line_length 16 \
+  --input "$meta_resources_dir/test_data/input.fa" \
+  --name_list "$meta_resources_dir/test_data/list.lst" \
+  --output "sub_sampled.fa"
