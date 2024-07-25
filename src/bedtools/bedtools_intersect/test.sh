a760fb18c2c97d2e2a70979730db2de19043feb8
tgaspe <theodorogtc@gmail.com>
2024-07-25 11:01:26 -0300

    Update test.sh

--- src/bedtools/bedtools_intersect/test.sh
+++ src/bedtools/bedtools_intersect/test.sh
@@ -13,10 +13,13 @@ echo "Creating Test Data..."
 mkdir test_data
 
 # Create and populate input_a.bed
+printf "chr1\t100\t200\nchr1\t150\t250\nchr1\t300\t400" > "test_data/input_a.bed"
 
 # Create and populate input_b.bed
+printf "chr1\t180\t280\nchr1\t290\t390\nchr1\t500\t600" > "test_data/input_b.bed"
 
 # Create and populate expected.bed
+printf "chr1\t180\t200\nchr1\t180\t250\nchr1\t300\t390" > "test_data/expected.bed"
 
 # Run basic test
 mkdir test1
@@ -28,5 +31,5 @@ echo "> Run bedtools_intersect on BED files"
   --input_b "../test_data/input_b.bed" \
   --output "output.bed"
 
-
+# checks
 
