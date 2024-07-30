3676351fb66a5b7c2c7089807dac8e2444228e43
tgaspe <theodorogtc@gmail.com>
2024-07-30 15:50:08 -0300

    Config file and Script.sh

--- src/bedtools/bedtools_bamtofastq/script.sh
+++ src/bedtools/bedtools_bamtofastq/script.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
 
+## VIASH START
+## VIASH END
+
+# Unset parameters
+[[ "$par_tags" == "false" ]] && unset par_tags
+
+# Execute bedtools bamtofastq with the provided arguments
+bedtools bamtofastq \
+    ${par_tags:+-tags} \
+    -i "$par_input" \
+    -fq "$par_output_fq"
+    ${par_output_fq2:+-fq2 "$par_output_fq2"}
 
---
+++ src/bedtools/bedtools_bamtofastq/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#viash ns build -q bedtools_bamtofastq --setup cb
+
+
