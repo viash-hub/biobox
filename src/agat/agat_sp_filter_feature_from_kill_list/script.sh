6a482c7d969b48da50908b833316cd39d957704d
Leila011 <leilapaquay@gmail.com>
2024-07-31 13:47:27 +0200

    add tests

--- src/agat/agat_sp_filter_feature_from_kill_list/script.sh
+++ src/agat/agat_sp_filter_feature_from_kill_list/script.sh
@@ -9,8 +9,8 @@
 # run agat_sp_filter_feature_from_kill_list
 agat_sp_filter_feature_from_kill_list.pl \
   --gff "$par_gff" \
+  --kill_list "$par_kill_list" \
   --output "$par_output" \
-  --kl "$par_kl" \
   ${par_type:+--type "${par_type}"} \
   ${par_attribute:+--attribute "${par_attribute}"} \
   ${par_config:+--config "${par_config}"} \
