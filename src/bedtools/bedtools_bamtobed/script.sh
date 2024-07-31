c4899019bfcf73eda9d485b6fb8124b9fc1ac88c
tgaspe <theodorogtc@gmail.com>
2024-07-31 15:13:43 -0300

    Initial Commit

---
+++ src/bedtools/bedtools_bamtobed/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+# Unset parameters
+
+
+# Execute bedtools sort with the provided arguments
+bedtools bamtobed \
+
---
+++ src/bedtools/bedtools_bamtobed/test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+#############################################
+# helper functions
+assert_file_exists() {
+  [ -f "$1" ] || { echo "File '$1' does not exist" && exit 1; }
+}
+assert_file_not_empty() {
+  [ -s "$1" ] || { echo "File '$1' is empty but shouldn't be" && exit 1; }
+}
+assert_file_contains() {
+  grep -q "$2" "$1" || { echo "File '$1' does not contain '$2'" && exit 1; }
+}
+assert_identical_content() {
+  diff -a "$2" "$1" \
+    || (echo "Files are not identical!" && exit 1)
+}
+#############################################
+
+# Test 1: 
+mkdir test1
+cd test1
+
+
+
+cd ..
+
+
+echo "---- All tests succeeded! ----"
+exit 0
