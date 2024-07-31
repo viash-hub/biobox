11d6afa99c5efc1f3e1b6aff267c2c1b330685c2
Leila011 <leilapaquay@gmail.com>
2024-07-31 15:22:50 +0200

    add run script and handle multiple inputs

---
+++ src/agat/agat_sp_merge_annotations/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+# Convert a list of file names to multiple -gff arguments
+input_files=""
+IFS=";" read -ra file_names <<< "$par_gff"
+for file in "${file_names[@]}"; do
+    input_files+="-gff $file "
+done
+unset IFS
+
+# run agat_sp_merge_annotations
+agat_sp_merge_annotations.pl \
+  $input_files \
+  -o "$par_output" \
+  ${par_config:+--config "${par_config}"}
