df7c366a436a736787a1361c6f8e88eef6f5fb07
Leila011 <leilapaquay@gmail.com>
2024-07-31 13:43:28 +0200

    all fetch kill_list.txt

--- src/agat/agat_sp_filter_feature_from_kill_list/test_data/script.sh
+++ src/agat/agat_sp_filter_feature_from_kill_list/test_data/script.sh
@@ -8,3 +8,4 @@ fi
 # copy test data
 cp -r /tmp/agat_source/t/scripts_output/in/1.gff src/agat/agat_sp_filter_feature_from_kill_list/test_data
 cp -r /tmp/agat_source/t/scripts_output/out/agat_sp_filter_feature_from_kill_list_1.gff src/agat/agat_sp_filter_feature_from_kill_list/test_data
+cp -r /tmp/agat_source/t/scripts_output/in/kill_list.txt src/agat/agat_sp_filter_feature_from_kill_list/test_data
