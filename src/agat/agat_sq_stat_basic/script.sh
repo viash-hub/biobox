061b593597a66929679218f7f368c0491c9a1533
Leila011 <leilapaquay@gmail.com>
2024-08-01 17:19:38 +0200

    handle input --gff has multiple=true

--- src/agat/agat_sq_stat_basic/script.sh
+++ src/agat/agat_sq_stat_basic/script.sh
@@ -6,6 +6,14 @@
 # unset flags
 [[ "$par_inflate" == "false" ]] && unset par_inflate
 
+# Convert a list of file names to multiple -gff arguments
+input_files=""
+IFS=";" read -ra file_names <<< "$par_gff"
+for file in "${file_names[@]}"; do
+    input_files+="--gff $file "
+done
+unset IFS
+
 # take care of --genome (can originally be either a fasta file or an integer)
 if [[ -n "$par_genome_size" ]]; then
   genome_arg=$par_genome_size
@@ -15,8 +23,8 @@ fi
 
 # run agat_convert_sp_bed2gff.pl
 agat_sq_stat_basic.pl \
-  --gff "$par_gff" \
+  $input_files \
   ${genome_arg:+--genome "${genome_arg}"} \
-  -o "$par_output" \
+  --output "${par_output}" \
   ${par_inflate:+--inflate} \
   ${par_config:+--config "${par_config}"}
