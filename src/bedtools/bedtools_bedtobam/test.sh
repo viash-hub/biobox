e7e1559385787636e1a4d1f5f0b1863e21552b08
tgaspe <theodorogtc@gmail.com>
2024-08-02 11:02:16 -0300

    adding identical check

--- src/bedtools/bedtools_bedtobam/test.sh
+++ src/bedtools/bedtools_bedtobam/test.sh
@@ -29,6 +29,11 @@ printf "chr1\t248956422\nchr3\t242193529\nchr2\t198295559\n" > "test_data/genome
 printf "chr2:172936693-172938111\t128\t228\tmy_read/1\t37\t+\nchr2:172936693-172938111\t428\t528\tmy_read/2\t37\t-\n" > "test_data/example.bed"
 printf "chr2:172936693-172938111\t128\t228\tmy_read/1\t60\t+\t128\t228\t255,0,0\t1\t100\t0\nchr2:172936693-172938111\t428\t528\tmy_read/2\t60\t-\t428\t528\t255,0,0\t1\t100\t0\n" > "test_data/example.bed12"
 
+# Expected umcompressed SAM files
+# printf "@HD\tVN:1.0\tSO:unsorted\n@PG\tID:BEDTools_bedToBam\tVN:Vv2.30.0\n@SQ\tSN:chr1\tAS:genome.txt\tLN:248956422\n@SQ\tSN:chr3\tAS:genome.txt\tLN:242193529\n@SQ\tSN:chr2\tAS:genome.txt\tLN:198295559\nmy_read/1\t0\tchr1\t129\t255\t100M\t*\t0\t0\t*\t*\nmy_read/2\t16\tchr1\t429\t255\t100M\t*\t0\t0\t*\t*\n" > "test_data/expected.sam"
+# printf "" > "test_data/expected12.sam"
+# printf "" > "test_data/expected_mapquality.sam"
+
 # Test 1: Default conversion BED to BAM
 mkdir test1
 cd test1
@@ -42,15 +47,15 @@ echo "> Run bedtools_bedtobam on BED file"
 # checks
 assert_file_exists "output.bam"
 assert_file_not_empty "output.bam"
+echo "- test1 succeeded -"
+
+cd ..
+
 # assert_identical_content # (bam file is compressed and -ubam option does not seem to work on this version.)
 # could use samtools view to check the content of the bam file, would need to add samtools as a dependency.
 # TODO: check if older versions works with -ubam option!!!
 # Version 2.27.1 (bedtools) -ubam outputs better than version 2.30.0 (bedtools).
 # However, its output is still somewhat compressed.
-
-echo "- test1 succeeded -"
-
-cd ..
 
 # Test 2: BED12 file
 mkdir test2
@@ -61,12 +66,13 @@ echo "> Run bedtools_bedtobam on BED12 file"
   --input "../test_data/example.bed12" \
   --genome "../test_data/genome.txt" \
   --output "output.bam" \
-  --bed12
+  --bed12 \
+#  --uncompress_bam
 
 # checks
 assert_file_exists "output.bam"
 assert_file_not_empty "output.bam"
-
+# assert_identical_content "output.bam" "../test_data/expected12.sam"
 echo "- test2 succeeded -"
 
 cd ..
@@ -85,6 +91,7 @@ echo "> Run bedtools_bedtobam on BED file with uncompressed BAM output"
 # checks
 assert_file_exists "output.bam"
 assert_file_not_empty "output.bam"
+# assert_identical_content "output.bam" "../test_data/expected.sam"
 
 echo "- test3 succeeded -"
 
@@ -99,12 +106,13 @@ echo "> Run bedtools_bedtobam on BED file with map quality"
   --input "../test_data/example.bed" \
   --genome "../test_data/genome.txt" \
   --output "output.bam" \
-  --map_quality 10
+  --map_quality 10 \
+#  --uncompress_bam
 
 # checks
 assert_file_exists "output.bam"
 assert_file_not_empty "output.bam"
-
+#assert_identical_content "output.bam" "../test_data/expected_mapquality.sam"
 echo "- test4 succeeded -"
 
 cd ..
