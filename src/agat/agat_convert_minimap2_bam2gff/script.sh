309ef68304409499574bd389b9bf131d19c3b114
Leila011 <leilapaquay@gmail.com>
2024-08-04 13:58:48 +0200

    add run script

---
+++ src/agat/agat_convert_minimap2_bam2gff/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+# unset flags
+[[ "$par_bam" == "false" ]] && unset par_bam
+[[ "$par_sam" == "false" ]] && unset par_sam
+
+# run agat_convert_sp_bed2gff.pl
+agat_convert_minimap2_bam2gff.pl \
+  --input "$par_input" \
+  --output "$par_output" \
+  ${par_bam:+--bam} \
+  ${par_sam:+--sam} \
+  ${par_config:+--config "${par_config}"}
