8e7b1c85305da11a551c514912eeacd2cc8b6c70
Leila011 <leilapaquay@gmail.com>
2024-08-04 14:49:37 +0200

    add run script

---
+++ src/agat/agat_convert_sp_gff2bed/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+agat_convert_sp_gff2bed.pl \
+  --gff "$par_gff" \
+  --output "$par_output" \
+  ${par_nc:+--nc "${par_nc}"} \
+  ${par_sub:+--sub "${par_sub}"} \
+  ${par_config:+--config "${par_config}"}
