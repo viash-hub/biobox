83b261bc8afaf0ae7a2e6489e4768fcc5b3e3da9
Leila011 <leilapaquay@gmail.com>
2024-08-04 15:04:47 +0200

    add test

---
+++ src/agat/agat_convert_sp_gff2bed/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+test_dir="${meta_resources_dir}/test_data"
+out_dir="${meta_resources_dir}/out_data"
+
+echo "> Run $meta_name with test data"
+"$meta_executable" \
+  --gff "$test_dir/1.gff" \
+  --output "$out_dir/output.gff" 
+
+echo ">> Checking output"
+[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+
+echo ">> Check if output is empty"
+[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+
+echo ">> Check if output matches expected output"
+diff "$out_dir/output.gff" "$test_dir/agat_convert_sp_gff2bed_1.gff"
+if [ $? -ne 0 ]; then
+  echo "Output file output.gff does not match expected output"
+  exit 1
+fi
+
+echo "> Test successful"
