789f15dd2b8d35efef9670178bcee0a527821232
Leila011 <leilapaquay@gmail.com>
2024-08-05 08:24:17 +0200

    fix run script

--- src/agat/agat_convert_sp_gff2tsv/script.sh
+++ src/agat/agat_convert_sp_gff2tsv/script.sh
@@ -3,7 +3,7 @@
 ## VIASH START
 ## VIASH END
 
-agat_convert_sp_gff2gtf.pl \
-  -f "$par_gff" \
-  -o "$par_output" \
+agat_convert_sp_gff2tsv.pl \
+  -gff "$par_gff" \
+  -output "$par_output" \
   ${par_config:+--config "${par_config}"}
