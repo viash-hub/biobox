1ed44dd45e6d28a66514c42aab8847b0d81836d0
Leila011 <leilapaquay@gmail.com>
2024-08-05 08:40:05 +0200

    add test data and expected output  + script to fetch them

---
+++ src/agat/agat_convert_sp_gff2zff/test_data/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# clone repo
+if [ ! -d /tmp/agat_source ]; then
+  git clone --depth 1 --single-branch --branch master https://github.com/NBISweden/AGAT /tmp/agat_source
+fi
+
+# copy test data
+cp -r /tmp/agat_source/t/scripts_output/in/1.gff src/agat/agat_convert_sp_gff2zff/test_data/
+cp -r /tmp/agat_source/t/scripts_output/in/1.fa src/agat/agat_convert_sp_gff2zff/test_data/
+cp -r /tmp/agat_source/t/scripts_output/out/agat_convert_sp_gff2zff_1.gff src/agat/agat_convert_sp_gff2zff/test_data/
