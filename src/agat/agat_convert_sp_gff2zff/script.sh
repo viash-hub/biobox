665b32510df1505fe4377e6d213434ed5a2bc29d
Leila011 <leilapaquay@gmail.com>
2024-08-05 08:53:25 +0200

    fix test & cleanup

--- src/agat/agat_convert_sp_gff2zff/script.sh
+++ src/agat/agat_convert_sp_gff2zff/script.sh
@@ -7,4 +7,4 @@ agat_convert_sp_gff2zff.pl \
   --gff "$par_gff" \
   --fasta "$par_fasta" \
   -output "$par_output" \
-  ${par_config:+--config "${par_config}"} \
+  ${par_config:+--config "${par_config}"} 
