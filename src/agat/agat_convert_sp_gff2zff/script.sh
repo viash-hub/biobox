67631b702181bd0461bc1cf514a0d6079254398b
Leila011 <leilapaquay@gmail.com>
2024-08-05 08:38:46 +0200

    add run script

---
+++ src/agat/agat_convert_sp_gff2zff/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+agat_convert_sp_gff2zff.pl \
+  --gff "$par_gff" \
+  --fasta "$par_fasta" \
+  -output "$par_output" \
+  ${par_config:+--config "${par_config}"} \
