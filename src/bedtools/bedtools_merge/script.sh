fb43c2fb0cff393f866b28b407bb9a83fabf7e7e
tgaspe <theodorogtc@gmail.com>
2024-08-05 10:36:03 -0300

    strand option tests

--- src/bedtools/bedtools_merge/script.sh
+++ src/bedtools/bedtools_merge/script.sh
@@ -5,23 +5,20 @@
 
 # Unset parameters
 [[ "$par_strand" == "false" ]] && unset par_strand
-[[ "$par_specific_strand" == "false" ]] && unset par_specific_strand
 [[ "$par_bed" == "false" ]] && unset par_bed
 [[ "$par_header" == "false" ]] && unset par_header
 [[ "$par_no_buffer" == "false" ]] && unset par_no_buffer
 
-
-
 # Execute bedtools merge with the provided arguments
 bedtools merge \
     ${par_strand:+-s} \
-    ${par_specific_strand:+-S} \
+    ${par_specific_strand:+-S "$par_specific_strand"} \
     ${par_bed:+-bed} \
     ${par_header:+-header} \
     ${par_no_buffer:+-nobuf} \
     ${par_distance:+-d "$par_distance"} \
     ${par_columns:+-c "$par_columns"} \
-    ${par_operation:+-0 "$par_operation"} \
+    ${par_operation:+-o "$par_operation"} \
     ${par_delimeter:+-delim "$par_delimeter"} \
     ${par_precision:+-prec "$par_precision"} \
     -i "$par_input" \
--- src/bedtools/bedtools_merge/test.sh
+++ src/bedtools/bedtools_merge/test.sh
@@ -30,25 +30,92 @@ echo "Creating Test Data..."
 mkdir -p test_data
 
 # Create and populate example files
+printf "chr1\t100\t200\nchr1\t150\t250\nchr1\t300\t400\n" > "test_data/featureA.bed"
+printf "chr1\t100\t200\ta1\t1\t+\nchr1\t180\t250\ta2\t2\t+\nchr1\t250\t500\ta3\t3\t-\nchr1\t501\t1000\ta4\t4\t+\n" > "test_data/featureB.bed"
+
+# Create and populate example.gff file
+printf "chr1\t.\tgene\t1000\t2000\t.\t+\t.\tID=gene1;Name=Gene1\n" >> "test_data/expected_sorted.gff"
+printf "chr1\t.\texon\t1000\t1200\t.\t+\t.\tID=exon1;Parent=transcript1\n" >> "test_data/expected_sorted.gff"
+printf "chr1\t.\tCDS\t1000\t1200\t.\t+\t0\tID=cds1;Parent=transcript1\n" >> "test_data/expected_sorted.gff"
+printf "chr1\t.\tCDS\t1500\t1700\t.\t+\t2\tID=cds2;Parent=transcript1\n" >> "test_data/expected_sorted.gff"
+printf "chr2\t.\texon\t1500\t1700\t.\t+\t.\tID=exon2;Parent=transcript1\n" >> "test_data/expected_sorted.gff"
+printf "chr3\t.\tmRNA\t1000\t2000\t.\t+\t.\tID=transcript1;Parent=gene1\n" >> "test_data/expected_sorted.gff"
 
 # Create expected output files
+printf "chr1\t100\t250\nchr1\t300\t400\n" > "test_data/expected.bed"
+printf "chr1\t100\t250\nchr1\t250\t500\nchr1\t501\t1000\n" > "test_data/expected_strand.bed"
+printf "chr1\t100\t250\nchr1\t501\t1000\n" > "test_data/expected_specific_strand.bed"
 
 # Test 1: Default sort on BED file
 mkdir test1
 cd test1
 
 echo "> Run bedtools_merge on BED file"
+"$meta_executable" \
+  --input "../test_data/featureA.bed" \
+  --output "output.bed"
+
+# # checks
+assert_file_exists "output.bed"
+assert_file_not_empty "output.bed"
+assert_identical_content "output.bed" "../test_data/expected.bed"
+echo "- test1 succeeded -"
+
+cd ..
+
+# Test 2: strand option
+mkdir test2
+cd test2
+
+echo "> Run bedtools_merge on BED file with strand option"
+"$meta_executable" \
+  --input "../test_data/featureB.bed" \
+  --output "output.bed" \
+  --strand
+
+# checks
+assert_file_exists "output.bed"
+assert_file_not_empty "output.bed"
+assert_identical_content "output.bed" "../test_data/expected_strand.bed"
+echo "- test2 succeeded -"
+
+cd ..
+
+# Test 3: specific strand option
+mkdir test3
+cd test3
+
+echo "> Run bedtools_merge on BED file with specific strand option"
+"$meta_executable" \
+  --input "../test_data/featureB.bed" \
+  --output "output.bed" \
+  --specific_strand "+" 
+
+# checks
+assert_file_exists "output.bed"
+assert_file_not_empty "output.bed"
+assert_identical_content "output.bed" "../test_data/expected_specific_strand.bed"
+echo "- test3 succeeded -"
+
+cd ..
+
+# Test 4: BED option
+# mkdir test4
+# cd test4
+
+# echo "> Run bedtools_merge on BED file with BED option"
 # "$meta_executable" \
 #   --input "../test_data/featureA.bed" \
-#   --output "output.bed"
+#   --output "output.bed" \
+#   --bed
 
 # # checks
 # assert_file_exists "output.bed"
 # assert_file_not_empty "output.bed"
-# assert_identical_content "output.bed" "../test_data/expected_sorted_A.bed"
-# echo "- test1 succeeded -"
+# assert_identical_content "output.bed" "../test_data/expected_bed.bed"
+# echo "- test4 succeeded -"
 
-cd ..
+# cd ..
 
 
 
