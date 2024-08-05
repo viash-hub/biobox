03ddb323d18e3e569c3b2ad5fa8e6e9318fb9c34
sumanm99 <sumanmuralidharan1999@gmail.com>
2024-08-05 14:58:01 +0530

    Testing NanoPlot in CLI

--- src/nanoplot/test_data/script.sh
+++ src/nanoplot/test_data/script.sh
@@ -1,7 +1,6 @@
-echo -e "@SEQ_ID\nGATTTGGGGTTTTCCCAGTT\n+\n\!\!\!FFFEEEEDDDCCCGGGA@" > test.fastq
+echo -e "@SEQ_ID\nGATTTGGGGTTTTCCCAGTT\n+\n\!\!\!FFFEEEEDDDCCCGGGA@" > ./src/nanoplot/test_data/test.fastq
 
-echo -e ">SEQ_ID\nGATTTGGGGTTTTCCCAGTT" > test.fasta
-
+echo -e ">SEQ_ID\nGATTTGGGGTTTTCCCAGTT" > ./src/nanoplot/test_data/test.fasta
 # echo -e ">ref\nGATTTGGGGTTTTCCCAGTT" > ref.fasta
 # bwa index ref.fasta
 # echo -e "@SEQ_ID\nGATTTGGGGTTTTCCCAGTT\n+\n'!!!FFFEEEEDDDCCCGGGA@'" > read.fastq
@@ -10,4 +9,4 @@ echo -e ">SEQ_ID\nGATTTGGGGTTTTCCCAGTT" > test.fasta
 # samtools sort test.bam -o test_sorted.bam
 # samtools index test_sorted.bam
 
-echo -e "filename\tread_id\tlength\tnum_events\tnum_events_insert\tnum_events_delete\nread.fastq\tread_1\t1000\t5000\t100\t50" > test_summary.txt
+echo -e "filename\tread_id\tlength\tnum_events\tnum_events_insert\tnum_events_delete\nread.fastq\tread_1\t1000\t5000\t100\t50" > ./src/nanoplot/test_data/test_summary.txt
