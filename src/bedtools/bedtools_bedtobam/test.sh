53fe29a454983cba1659519f52cd89201d8e2fa5
tgaspe <theodorogtc@gmail.com>
2024-08-06 10:28:17 -0300

    removing test_data

--- src/bedtools/bedtools_bedtobam/test.sh
+++ src/bedtools/bedtools_bedtobam/test.sh
@@ -37,12 +37,6 @@ printf "chr2\t.\texon\t1500\t1700\t.\t+\t.\tID=exon2;Parent=transcript1\n" >> "t
 printf "chr1\t.\tCDS\t1000\t1200\t.\t+\t0\tID=cds1;Parent=transcript1\n" >> "test_data/example.gff"
 printf "chr1\t.\tCDS\t1500\t1700\t.\t+\t2\tID=cds2;Parent=transcript1\n" >> "test_data/example.gff"
 
-# Expected umcompressed SAM files
-# printf "@HD\tVN:1.0\tSO:unsorted\n@PG\tID:BEDTools_bedToBam\tVN:Vv2.30.0\n@SQ\tSN:chr1\tAS:genome.txt\tLN:248956422\n@SQ\tSN:chr3\tAS:genome.txt\tLN:242193529\n@SQ\tSN:chr2\tAS:genome.txt\tLN:198295559\nmy_read/1\t0\tchr1\t129\t255\t100M\t*\t0\t0\t*\t*\nmy_read/2\t16\tchr1\t429\t255\t100M\t*\t0\t0\t*\t*\n" > "test_data/expected.sam"
-# printf "" > "test_data/expected12.sam"
-# printf "" > "test_data/expected_mapquality.sam"
-# printf "" > "test_data/expected_gff.sam"
-
 # Test 1: Default conversion BED to BAM
 mkdir test1
 cd test1
@@ -60,7 +54,7 @@ echo "- test1 succeeded -"
 
 cd ..
 
-# assert_identical_content # (bam file is compressed and -ubam option does not seem to work on this version.)
+# I could not assert_identical_content (bam file is compressed and -ubam option does not seem to work on this version).
 # could use samtools view to check the content of the bam file, would need to add samtools as a dependency.
 # TODO: check if older versions works with -ubam option!!!
 # Version 2.27.1 (bedtools) -ubam outputs better than version 2.30.0 (bedtools).
@@ -135,6 +129,7 @@ echo "> Run bedtools_bedtobam on GFF file"
   --input "../test_data/example.gff" \
   --genome "../test_data/genome.txt" \
   --output "output.bam"
+#  --uncompress_bam
 
 # checks
 assert_file_exists "output.bam"
