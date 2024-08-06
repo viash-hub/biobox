70ba7ad2de2da2980d88ce04f361dd1a2863e128
Leila011 <leilapaquay@gmail.com>
2024-08-06 17:52:15 +0200

    add run script

---
+++ src/agat/agat_sp_alignment_output_style/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+# unset flags
+[[ "$par_verbose" == "false" ]] && unset par_verbose
+
+# run agat_sp_alignment_output_style.pl
+agat_sp_alignment_output_style.pl \
+  --gff "$par_gff" \
+  --output "$par_output" \
+  ${par_verbose:+--verbose} \
+  ${par_config:+--config "${par_config}"}
