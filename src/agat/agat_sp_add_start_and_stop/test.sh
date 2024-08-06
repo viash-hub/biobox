b3a45ee1379f4258590e6ac7840cf5666ed90e6c
Leila011 <leilapaquay@gmail.com>
2024-08-06 15:33:53 +0200

    fix test: add `--ni` flag

--- src/agat/agat_sp_add_start_and_stop/test.sh
+++ src/agat/agat_sp_add_start_and_stop/test.sh
@@ -6,12 +6,13 @@
 test_dir="${meta_resources_dir}/test_data"
 out_dir="${meta_resources_dir}/out_data"
 
-echo "> Run $meta_name with test data"
+echo "> Run $meta_name with test data and ni flag"
 "$meta_executable" \
   --gff "$test_dir/agat_sp_add_start_and_stop.gff" \
   --fasta "$test_dir/1.fa" \
-  --output "$out_dir/output.gff"
-
+  --output "$out_dir/output.gff" \
+  --ni 
+    
 echo ">> Checking output"
 [ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
 
