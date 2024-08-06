1e815eb373474f72632c58468529ec09d21c92cc
Leila011 <leilapaquay@gmail.com>
2024-08-06 15:34:16 +0200

    add second test: use `--extend` flag

--- src/agat/agat_sp_add_start_and_stop/test.sh
+++ src/agat/agat_sp_add_start_and_stop/test.sh
@@ -26,4 +26,30 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+rm -f "$out_dir/output.gff"
+
+echo "> Run $meta_name with test data and ni flag"
+"$meta_executable" \
+  --gff "$test_dir/agat_sp_add_start_and_stop.gff" \
+  --fasta "$test_dir/1.fa" \
+  --output "$out_dir/output.gff" \
+  --ni \
+  --extend
+
+  
+echo ">> Checking output"
+[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+
+echo ">> Check if output is empty"
+[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+
+echo ">> Check if output matches expected output"
+diff "$out_dir/output.gff" "$test_dir/agat_sp_add_start_and_stop_2.gff"
+if [ $? -ne 0 ]; then
+  echo "Output file output.gff does not match expected output"
+  exit 1
+fi
+
+rm -f "$out_dir/output.gff"
+
 echo "> Test successful"
--- src/agat/agat_sp_add_start_and_stop/test_data/script.sh
+++ src/agat/agat_sp_add_start_and_stop/test_data/script.sh
@@ -9,3 +9,4 @@ fi
 cp -r /tmp/agat_source/t/scripts_output/in/agat_sp_add_start_and_stop.gff src/agat/agat_sp_add_start_and_stop/test_data/
 cp -r /tmp/agat_source/t/scripts_output/in/1.fa src/agat/agat_sp_add_start_and_stop/test_data/
 cp -r /tmp/agat_source/t/scripts_output/out/agat_sp_add_start_and_stop_1.gff src/agat/agat_sp_add_start_and_stop/test_data/
+cp -r /tmp/agat_source/t/scripts_output/out/agat_sp_add_start_and_stop_2.gff src/agat/agat_sp_add_start_and_stop/test_data/
