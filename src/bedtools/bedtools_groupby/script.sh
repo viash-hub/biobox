3f8a45f67bbbf97184b237f462838226967fae3d
tgaspe <theodorogtc@gmail.com>
2024-08-06 12:00:29 -0300

    script.sh

--- src/bedtools/bedtools_groupby/script.sh
+++ src/bedtools/bedtools_groupby/script.sh
@@ -3,8 +3,24 @@
 ## VIASH START
 ## VIASH END
 
+# Unset parameters
+[[ "$par_full" == "false" ]] && unset par_full
+[[ "$par_inheader" == "false" ]] && unset par_inheader
+[[ "$par_outheader" == "false" ]] && unset par_outheader
+[[ "$par_header" == "false" ]] && unset par_header
+[[ "$par_ignorecase" == "false" ]] && unset par_ignorecase
 
 bedtools groupby \
+    ${par_full:+-full} \
+    ${par_inheader:+-inheader} \
+    ${par_outheader:+-outheader} \
+    ${par_header:+-header} \
+    ${par_ignorecase:+-ignorecase} \
+    ${par_precision:+-prec "$par_precision"} \
+    ${par_delimiter:+-delim "$par_delimiter"} \
     -i "$par_input" \
+    -g "$par_groupby" \
+    -c "$par_column" \
+    -o "$par_operation" \
     > "$par_output"
     
