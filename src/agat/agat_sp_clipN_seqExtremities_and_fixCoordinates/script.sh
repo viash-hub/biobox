831370594ae6d2b7e087a5e2a91ad02318e9a27f
Leila011 <leilapaquay@gmail.com>
2024-08-07 09:03:30 +0200

    add run script

---
+++ src/agat/agat_sp_clipN_seqExtremities_and_fixCoordinates/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+agat_sp_clipN_seqExtremities_and_fixCoordinates.pl \
+  --gff "$par_gff" \
+  --fasta "$par_fasta" \
+  --of "$par_output_fasta" \
+  --og "$par_output_gff" \
+  ${par_config:+--config "${par_config}"}
