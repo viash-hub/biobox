da6e11fb8045311e8614e8c7dfb4005b46669f78
tgaspe <theodorogtc@gmail.com>
2024-08-07 10:51:31 -0300

    Update script.sh

--- src/bedtools/bedtools_genomecov/script.sh
+++ src/bedtools/bedtools_genomecov/script.sh
@@ -4,9 +4,37 @@
 ## VIASH END
 
 [[ "$par_input_bam" == "false" ]] && unset par_input_bam
+[[ "$par_depth" == "false" ]] && unset par_depth
+[[ "$par_depth_zero" == "false" ]] && unset par_depth_zero
+[[ "$par_bed_graph" == "false" ]] && unset par_bed_graph
+[[ "$par_bed_graph_zero_coverage" == "false" ]] && unset par_bed_graph_zero_coverage
+[[ "$par_split" == "false" ]] && unset par_split
+[[ "$par_ignore_deletion" == "false" ]] && unset par_ignore_deletion
+[[ "$par_pair_end_coverage" == "false" ]] && unset par_pair_end_coverage
+[[ "$par_fragment_size" == "false" ]] && unset par_fragment_size
+[[ "$par_du" == "false" ]] && unset par_du
+[[ "$par_5_prime" == "false" ]] && unset par_5_prime
+[[ "$par_3_prime" == "false" ]] && unset par_3_prime
+[[ "$par_trackline" == "false" ]] && unset par_trackline
 
 bedtools genomecov \
     ${par_input_bam:+-ibam "$par_input_bam"} \
+    ${par_depth:+-d} \
+    ${par_depth_zero:+-dz} \
+    ${par_bed_graph:+-bg} \
+    ${par_bed_graph_zero_coverage:+-bga} \
+    ${par_split:+-split} \
+    ${par_ignore_deletion:+-ignoreD} \
+    ${par_pair_end_coverage:+-pc} \
+    ${par_fragment_size:+-fs} \
+    ${par_du:+-du} \
+    ${par_5_prime:+-5} \
+    ${par_3_prime:+-3} \
+    ${par_trackline:+-trackline} \
+    ${par_strand:+-strand "$par_strand"} \
+    ${par_max:+-max "$par_max"} \
+    ${par_scale:+-scale "$par_scale"} \
+    ${par_trackopts:+-trackopts "$par_trackopts"} \
     -i "$par_input" \
     -g "$par_genome" \
     > "$par_output"
