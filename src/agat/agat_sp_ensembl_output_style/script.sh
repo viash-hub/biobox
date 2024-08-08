654efa45d149fd9694abae1d1414861b6486fa3a
Leila011 <leilapaquay@gmail.com>
2024-08-08 09:57:50 +0200

    fix run script

--- src/agat/agat_sp_ensembl_output_style/script.sh
+++ src/agat/agat_sp_ensembl_output_style/script.sh
@@ -6,9 +6,9 @@
 # unset flags
 [[ "$par_verbose" == "false" ]] && unset par_verbose
 
-# run agat_convert_sp_bed2gff.pl
-agat_convert_bed2gff.pl \
-  --gff "$par_bed" \
+# run agat_sp_ensembl_output_style.pl
+agat_sp_ensembl_output_style.pl \
+  --gff "$par_gff" \
   --output "$par_output" \
   ${par_verbose:+-v}
   ${par_config:+--config "${par_config}"} \
