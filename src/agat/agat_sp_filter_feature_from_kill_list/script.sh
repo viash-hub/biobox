4819514d1a3414dbedcc5f052de830350363e2ec
Leila011 <leilapaquay@gmail.com>
2024-08-19 11:04:03 +0200

    convert par_type to comma separated list

--- src/agat/agat_sp_filter_feature_from_kill_list/script.sh
+++ src/agat/agat_sp_filter_feature_from_kill_list/script.sh
@@ -6,6 +6,9 @@
 # unset flags
 [[ "$par_verbose" == "false" ]] && unset par_verbose
 
+# convert par_type to comma separated list
+par_type=$(echo $par_type | tr ';' ',').
+
 # run agat_sp_filter_feature_from_kill_list
 agat_sp_filter_feature_from_kill_list.pl \
   --gff "$par_gff" \
