d63e2674e821ce8890a5f7880ee1f617f75e7625
Leila011 <leilapaquay@gmail.com>
2024-08-19 10:43:31 +0200

    create temporary directory and clean up on exit

--- src/agat/agat_convert_genscan2gff/test.sh
+++ src/agat/agat_convert_genscan2gff/test.sh
@@ -4,21 +4,27 @@
 ## VIASH END
 
 test_dir="${meta_resources_dir}/test_data"
-out_dir="${meta_resources_dir}/out_data"
+
+# create temporary directory and clean up on exit
+TMPDIR=$(mktemp --tmpdir "$meta_temp_dir")
+function clean_up {
+ rm -rf "$TMPDIR"
+}
+trap clean_up EXIT
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
   --genscan "$test_dir/test.genscan" \
-  --output "$out_dir/output.gff" 
+  --output "$TMPDIR/output.gff" 
 
 echo ">> Checking output"
-[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+[ ! -f "$TMPDIR/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+[ ! -s "$TMPDIR/output.gff" ] && echo "Output file output.gff is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$out_dir/output.gff" "$test_dir/agat_convert_genscan2gff_1.gff"
+diff "$TMPDIR/output.gff" "$test_dir/agat_convert_genscan2gff_1.gff"
 if [ $? -ne 0 ]; then
   echo "Output file output.gff does not match expected output"
   exit 1
