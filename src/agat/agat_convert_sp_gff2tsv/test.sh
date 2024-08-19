526f23875e338c2b1f08fae904961f9dd21a1fd0
Leila011 <leilapaquay@gmail.com>
2024-08-19 14:53:54 +0200

    create temporary directory and clean up on exit

--- src/agat/agat_convert_sp_gff2tsv/test.sh
+++ src/agat/agat_convert_sp_gff2tsv/test.sh
@@ -6,21 +6,27 @@ set -eo pipefail
 ## VIASH END
 
 test_dir="${meta_resources_dir}/test_data"
-out_dir="${meta_resources_dir}/out_data"
+
+# create temporary directory and clean up on exit
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+function clean_up {
+ [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR"
+}
+trap clean_up EXIT
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
   --gff "$test_dir/1.gff" \
-  --output "$out_dir/output.tsv" 
+  --output "$TMPDIR/output.tsv" 
 
 echo ">> Checking output"
-[ ! -f "$out_dir/output.tsv" ] && echo "Output file output.tsv does not exist" && exit 1
+[ ! -f "$TMPDIR/output.tsv" ] && echo "Output file output.tsv does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$out_dir/output.tsv" ] && echo "Output file output.tsv is empty" && exit 1
+[ ! -s "$TMPDIR/output.tsv" ] && echo "Output file output.tsv is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$out_dir/output.tsv" "$test_dir/agat_convert_sp_gff2tsv_1.tsv"
+diff "$TMPDIR/output.tsv" "$test_dir/agat_convert_sp_gff2tsv_1.tsv"
 if [ $? -ne 0 ]; then
   echo "Output file output.tsv does not match expected output"
   exit 1
