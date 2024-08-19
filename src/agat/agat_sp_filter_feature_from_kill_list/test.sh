409c13c569cea4d0576e7d6cf55e0ad9b3284766
Leila011 <leilapaquay@gmail.com>
2024-08-19 13:59:25 +0200

    fix create temporary directory

--- src/agat/agat_sp_filter_feature_from_kill_list/test.sh
+++ src/agat/agat_sp_filter_feature_from_kill_list/test.sh
@@ -8,11 +8,11 @@ set -eo pipefail
 test_dir="${meta_resources_dir}/test_data"
 
 # create temporary directory and clean up on exit
-TMPDIR=$(mktemp -d --tmpdir "$meta_temp_dir")
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
 function clean_up {
- rm -rf "$TMPDIR"
+ [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR"
 }
-trap clean_up EXIT
+#trap clean_up EXIT
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
