f1354a3b8b1c6d52edb4857720501ccd5c0b2c95
Leila011 <leilapaquay@gmail.com>
2024-08-19 16:08:27 +0200

    add a new argument output_dir such that the output directory can be specified

--- src/agat/agat_convert_sp_gff2zff/script.sh
+++ src/agat/agat_convert_sp_gff2zff/script.sh
@@ -10,3 +10,10 @@ agat_convert_sp_gff2zff.pl \
   --fasta "$par_fasta" \
   --output "$par_output" \
   ${par_config:+--config "${par_config}"} 
+
+# move the output to the given directory
+if [ -n "$par_output_dir" ]; then
+  mkdir -p "$par_output_dir"
+  mv "$par_output".ann "$par_output_dir"/"$par_output".ann
+  mv "$par_output".dna "$par_output_dir"/"$par_output".dna 
+fi
--- src/agat/agat_convert_sp_gff2zff/test.sh
+++ src/agat/agat_convert_sp_gff2zff/test.sh
@@ -11,18 +11,19 @@ echo "> Run $meta_name with test data"
 "$meta_executable" \
   --gff "$test_dir/1.gff" \
   --fasta "$test_dir/1.fa" \
-  --output "output" 
+  --output "output" \
+  --output_dir "output_dir"
 
 echo ">> Checking output"
-[ ! -f "output.ann" ] && echo "Output file output.ann does not exist" && exit 1
-[ ! -f "output.dna" ] && echo "Output file output.dna does not exist" && exit 1
+[ ! -f "output_dir/output.ann" ] && echo "Output file output.ann does not exist" && exit 1
+[ ! -f "output_dir/output.dna" ] && echo "Output file output.dna does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "output.ann" ] && echo "Output file output.ann is empty" && exit 1
-[ ! -s "output.dna" ] && echo "Output file output.dna is empty" && exit 1
+[ ! -s "output_dir/output.ann" ] && echo "Output file output.ann is empty" && exit 1
+[ ! -s "output_dir/output.dna" ] && echo "Output file output.dna is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "output.ann" "$test_dir/agat_convert_sp_gff2zff_1.gff"
+diff "output_dir/output.ann" "$test_dir/agat_convert_sp_gff2zff_1.gff"
 if [ $? -ne 0 ]; then
   echo "Output file output.ann does not match expected output"
   exit 1
