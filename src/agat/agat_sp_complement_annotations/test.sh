3827b0cd4835754a9925d59e281cf2895d43dcf0
Leila011 <leilapaquay@gmail.com>
2024-08-19 17:20:47 +0200

    create temporary directory and clean up on exit

--- src/agat/agat_sp_complement_annotations/test.sh
+++ src/agat/agat_sp_complement_annotations/test.sh
@@ -6,43 +6,49 @@ set -eo pipefail
 ## VIASH END
 
 test_dir="${meta_resources_dir}/test_data"
-out_dir="${meta_resources_dir}/out_data"
+
+# create temporary directory and clean up on exit
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+function clean_up {
+ [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR"
+}
+trap clean_up EXIT
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
   --ref "$test_dir/25_test.gff" \
   --add "$test_dir/9_test.gff" \
-  --output "$out_dir/output.gff" 
+  --output "$TMPDIR/output.gff" 
 
 echo ">> Checking output"
-[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+[ ! -f "$TMPDIR/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+[ ! -s "$TMPDIR/output.gff" ] && echo "Output file output.gff is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$out_dir/output.gff" "$test_dir/agat_sp_complement_annotations_1.gff"
+diff "$TMPDIR/output.gff" "$test_dir/agat_sp_complement_annotations_1.gff"
 if [ $? -ne 0 ]; then
   echo "Output file output.gff does not match expected output"
   exit 1
 fi
 
-rm -rf "$out_dir/output.gff"
+rm -rf "$TMPDIR/output.gff"
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
   --ref "$test_dir/agat_sp_complement_annotations_ref.gff" \
   --add "$test_dir/agat_sp_complement_annotations_add.gff" \
-  --output "$out_dir/output.gff" 
+  --output "$TMPDIR/output.gff" 
 
 echo ">> Checking output"
-[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+[ ! -f "$TMPDIR/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+[ ! -s "$TMPDIR/output.gff" ] && echo "Output file output.gff is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$out_dir/output.gff" "$test_dir/agat_sp_complement_annotations_2.gff"
+diff "$TMPDIR/output.gff" "$test_dir/agat_sp_complement_annotations_2.gff"
 if [ $? -ne 0 ]; then
   echo "Output file output.gff does not match expected output"
   exit 1
