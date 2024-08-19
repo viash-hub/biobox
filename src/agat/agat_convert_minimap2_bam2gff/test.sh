c5279753c49004ff123a73526eefbcf00284b232
Leila011 <leilapaquay@gmail.com>
2024-08-19 14:32:45 +0200

    create temporary directory and clean up on exit

--- src/agat/agat_convert_minimap2_bam2gff/test.sh
+++ src/agat/agat_convert_minimap2_bam2gff/test.sh
@@ -6,21 +6,27 @@ set -eo pipefail
 ## VIASH END
 
 test_dir="${meta_resources_dir}/test_data"
-out_dir="${meta_resources_dir}/out_data"
+
+# create temporary directory and clean up on exit
+TMPDIR=$(mktemp -d "$meta_temp_dir/$meta_functionality_name-XXXXXX")
+function clean_up {
+ [[ -d "$TMPDIR" ]] && rm -rf "$TMPDIR"
+}
+trap clean_up EXIT
 
 echo "> Run $meta_name with test data"
 "$meta_executable" \
   --input "$test_dir/test_minimap2.sam" \
-  --output "$out_dir/output.gff" 
+  --output "$TMPDIR/output.gff" 
 
 echo ">> Checking output"
-[ ! -f "$out_dir/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
+[ ! -f "$TMPDIR/output.gff" ] && echo "Output file output.gff does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$out_dir/output.gff" ] && echo "Output file output.gff is empty" && exit 1
+[ ! -s "$TMPDIR/output.gff" ] && echo "Output file output.gff is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$out_dir/output.gff" "$test_dir/agat_convert_minimap2_bam2gff_1.gff"
+diff "$TMPDIR/output.gff" "$test_dir/agat_convert_minimap2_bam2gff_1.gff"
 if [ $? -ne 0 ]; then
   echo "Output file output.gff does not match expected output"
   exit 1
