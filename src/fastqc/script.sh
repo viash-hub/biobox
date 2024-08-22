0661bd57c739685201cbb20d702e84fec0b944c4
tgaspe <theodorogtc@gmail.com>
2024-08-22 10:26:33 +0200

    Required Changes
    - large changes on script.sh

--- src/fastqc/script.sh
+++ src/fastqc/script.sh
@@ -6,6 +6,12 @@
 # exit on error
 set -eo pipefail
 
+# Check if both outputs are empty, at least one must be passed.
+if [[ -z "$par_html" ]] && [[ -z "$par_zip" ]]; then
+  echo "Error: At least one of the output arguments (--html or --zip) must be passed."
+  exit 1
+fi
+
 # unset flags
 unset_if_false=(
   par_casava
@@ -22,17 +28,21 @@ for par in ${unset_if_false[@]}; do
     [[ "$test_val" == "false" ]] && unset $par
 done
 
+tmpdir=$(mktemp -d "${meta_temp_dir}/${meta_name}-XXXXXXXX")
+function clean_up {
+  rm -rf "$tmpdir"
+}
+trap clean_up EXIT
+
 # Create input array 
 IFS=";" read -ra input <<< $par_input
 
 # Run fastqc
 fastqc \
-  ${par_outdir:+--outdir "$par_outdir"} \
   ${par_casava:+--casava} \
   ${par_nano:+--nano} \
   ${par_nofilter:+--nofilter} \
   ${par_extract:+--extract} \
-  ${par_java:+--java "$par_java"} \
   ${par_noextract:+--noextract} \
   ${par_nogroup:+--nogroup} \
   ${par_min_length:+--min_length "$par_min_length"} \
@@ -44,52 +54,27 @@ fastqc \
   ${par_quiet:+--quiet} \
   ${meta_cpus:+--threads "$meta_cpus"} \
   ${meta_temp_dir:+--dir "$meta_temp_dir"} \
-  ${par_input:+ ${input[*]}}
+  --outdir "${tmpdir}" \
+  "${input[@]}"
 
-# Get input directory
-input_dir=$(dirname ${input[0]})
-
-# Moves extracted output directory to current directory
-if [[ -n "$par_extract" ]]; then
-  mv "$input_dir/"*_fastqc $(pwd)
-fi
-
-# Both outputs args passed (html and zip)
-if [[ -n "$par_html" ]] && [[ -n "$par_zip" ]]; then
-  for i in "${!input[@]}"; do
-    # Substitute .bam, .sam, .bam_mapped, .sam_mapped, and .fastq to .fq
-    input_html=$(echo ${input[$i]} | sed 's/\(\.bam\|\.sam\|\.bam_mapped\|\.sam_mapped\|\.fastq\)/.fq/g') 
-    sample_name=$(basename $input_html .fq)
-    input_zip="$input_dir/${sample_name}_fastqc.zip"
-    input_html="$input_dir/${sample_name}_fastqc.html"
-    zip_file="${par_zip//\*/$sample_name}"
+# Move output files
+for file in "${input[@]}"; do
+  file_dir=$(dirname "$file")
+  # Removes everthing after the first dot of the basename
+  sample_name=$(basename "${file}" | sed 's/\..*$//')
+  if [[ -n "$par_html" ]]; then
+    input_html="$tempdir/${sample_name}_fastqc.html"
     html_file="${par_html//\*/$sample_name}"
-    mv "$input_zip" "$zip_file"
     mv "$input_html" "$html_file"
-  done
-# Only html output arg passed
-elif [[ -n "$par_html" ]]; then
-  for i in "${!input[@]}"; do
-    # Substitute .bam, .sam, .bam_mapped, .sam_mapped, and .fastq to .fq
-    input_html=$(echo ${input[$i]} | sed 's/\(\.bam\|\.sam\|\.bam_mapped\|\.sam_mapped\|\.fastq\)/.fq/g') 
-    sample_name=$(basename $input_html .fq)
-    input_html="$input_dir/${sample_name}_fastqc.html"
-    html_file="${par_html//\*/$sample_name}" # Substitute * with sample name
-    mv "$input_html" "$html_file"
-  done
-  rm "$input_dir"/*.zip
-# Only zip output arg passed
-elif [[ -n "$par_zip" ]]; then
-  for i in "${!input[@]}"; do
-    # Substitute .bam, .sam, .bam_mapped, .sam_mapped, and .fastq to .fq
-    input_zip=$(echo ${input[$i]} | sed 's/\(\.bam\|\.sam\|\.bam_mapped\|\.sam_mapped\|\.fastq\)/.fq/g') 
-    sample_name=$(basename $input_zip .fq)
-    input_zip="$input_dir/${sample_name}_fastqc.zip"
-    zip_file="${par_zip//\*/$sample_name}" # Substitute * with sample name
+  fi
+  if [[ -n "$par_zip" ]]; then
+    input_zip="$tempdir/${sample_name}_fastqc.zip"
+    zip_file="${par_zip//\*/$sample_name}"
     mv "$input_zip" "$zip_file"
-  done
-  rm "$input_dir"/*.html
-else 
-  mv "$input_dir"/*.html "$input_dir"/*.zip $(pwd)
-fi
-
+  fi
+  if [[ -n "$par_extract" ]]; then
+    # moves the extracted directory to the same directory as the input file
+    extract_dir="$tempdir/${sample_name}_fastqc"
+    mv "$extract_dir" "$file_dir"
+  fi
+done
