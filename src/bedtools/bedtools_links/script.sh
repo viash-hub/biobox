b0bf39b529f765b6b8cef54079b133c14f5008a6
tgaspe <theodorogtc@gmail.com>
2024-08-25 10:49:54 +0200

    Initial Commit

---
+++ src/bedtools/bedtools_links/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+set -eo pipefail
+
+# Execute bedtools links
+linksBed \
+    ${par_base_url:+-base "$par_base_url"} \
+    ${par_organism:+-org "$par_organism"} \
+    ${par_database:+-db "$par_database"} \
+    -i "$par_input" \
+    > "$par_output"
