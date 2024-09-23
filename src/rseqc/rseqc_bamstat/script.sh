a64fa69cc497208b648995ec98f37edd713525e4
emmarousseau <emmarou1@icloud.com>
2024-09-23 16:36:48 +0200

    add test, test data, version, help

--- src/rseqc/rseqc_bamstat/script.sh
+++ src/rseqc/rseqc_bamstat/script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+
 set -eo pipefail 
 
 bam_stat.py \
-    --input $par_input \
-    --mapq $par_map_qual \
+    --input "${par_input}" \
+    ${par_map_qual:+--mapq "${par_map_qual}"} \
 > $par_output
--- src/rseqc/rseqc_bamstat/test.sh
+++ src/rseqc/rseqc_bamstat/test.sh
@@ -9,15 +9,41 @@ output_summary="mapping_quality.txt"
 echo "> Running $meta_functionality_name."
 
 "$meta_executable" \
-    --input "$meta_resources_dir/$input_bam" \
+    --input "$meta_resources_dir/test_data/$input_bam" \
     --output "$output_summary"
 
 exit_code=$?
 [[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
 
-echo ">> Checking whether output can be found and has content"
-
+echo ">> Checking whether output is present"
 [ ! -f "$output_summary" ] && echo "$output_summary file missing" && exit 1
 [ ! -s "$output_summary" ] && echo "$output_summary file is empty" && exit 1
+
+echo ">> Checking whether output is correct"
+diff "$meta_resources_dir/test_data/ref_output.txt" "$meta_resources_dir/$output_summary" || { echo "Output is not correct"; exit 1; }
+
+#############################################################################
+
+echo ">>> Test 2: Test with non-default mapping quality threshold"
+
+output_summary="mapping_quality_mapq_30.txt"
+
+# run executable and tests
+echo "> Running $meta_functionality_name."
+
+"$meta_executable" \
+    --input "$meta_resources_dir/test_data/$input_bam" \
+    --output "$output_summary" \
+    --map_qual 50
+
+exit_code=$?
+[[ $exit_code != 0 ]] && echo "Non zero exit code: $exit_code" && exit 1
+
+echo ">> Checking whether output is present"
+[ ! -f "$output_summary" ] && echo "$output_summary file missing" && exit 1
+[ ! -s "$output_summary" ] && echo "$output_summary file is empty" && exit 1
+
+echo ">> Checking whether output is correct"
+diff "$meta_resources_dir/test_data/ref_output_mapq.txt" "$meta_resources_dir/$output_summary" || { echo "Output is not correct"; exit 1; }
 
 exit 0
