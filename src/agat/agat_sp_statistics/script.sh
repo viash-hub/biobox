6b259646887c9b6fed92abee394f51b3a24a427c
jakubmajercik <jakub.majercik@gmail.com>
2024-09-27 11:38:17 +0200

    PR fixes, extended unit tests

--- src/agat/agat_sp_statistics/script.sh
+++ src/agat/agat_sp_statistics/script.sh
@@ -8,12 +8,18 @@ set -eo pipefail
 # unset flags
 [[ "$par_d" == "false" ]] && unset par_d
 
+if [[ -n "$par_gs_size" && -n "$par_gs_fasta" ]]; then
+  echo "[error] Please provide only one of the following options to set genome size: --gs_size or --gs_fasta"
+  exit 1
+fi
+
 # run agat_sp_statistics
 agat_sp_statistics.pl \
   -i "$par_gff" \
   -o "$par_output" \
   ${par_plot:+-d} \
-  ${par_gs:+--gs "${par_gs}"} \
+  ${par_gs_size:+--gs "${par_gs_size}"} \
+  ${par_gs_fasta:+--gs "${par_gs_fasta}"} \
   ${par_verbose:+--verbose "${par_verbose}"} \
   ${par_config:+--config "${par_config}"}
 
--- src/agat/agat_sp_statistics/test.sh
+++ src/agat/agat_sp_statistics/test.sh
@@ -2,9 +2,6 @@
 
 set -eo pipefail
 
-## VIASH START
-## VIASH END
-
 test_dir="${meta_resources_dir}/test_data"
 
 # create temporary directory and clean up on exit
@@ -14,22 +11,55 @@ function clean_up {
 }
 trap clean_up EXIT
 
+cd "$TMPDIR"
+
+mkdir test1
+pushd test1
+
 echo "> Run $meta_name with test data and --emblmygff3"
 "$meta_executable" \
   --gff "$test_dir/1.gff" \
-  --output "$TMPDIR/output.txt" \
+  --output "output.txt" \
 
 echo ">> Checking output"
-[ ! -f "$TMPDIR/output.txt" ] && echo "Output file output.txt does not exist" && exit 1
+[ ! -f "output.txt" ] && echo "Output file output.txt does not exist" && exit 1
 
 echo ">> Check if output is empty"
-[ ! -s "$TMPDIR/output.txt" ] && echo "Output file output.txt is empty" && exit 1
+[ ! -s "output.txt" ] && echo "Output file output.txt is empty" && exit 1
 
 echo ">> Check if output matches expected output"
-diff "$TMPDIR/output.txt" "$test_dir/agat_sp_statistics_1.txt"
+diff "output.txt" "$test_dir/agat_sp_statistics_1.txt"
 if [ $? -ne 0 ]; then
   echo "Output file output.txt does not match expected output"
   exit 1
 fi
 
 echo "> Test successful"
+
+
+popd
+mkdir test2
+pushd test2
+
+cat <<EOF > genome.fasta
+>sample_sequence
+ATGCGTACGTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGCTAGC
+EOF
+
+echo "> Run $meta_name with both gs_size and gs_fasta"
+error_message=$("$meta_executable" \
+  --gff "$test_dir/1.gff" \
+  --output "output.txt" \
+  --gs_size "1000000" \
+  --gs_fasta "genome.fasta" 2>&1 || true)
+
+expected_error="[error] Please provide only one of the following options to set genome size: --gs_size or --gs_fasta"
+if [[ "$error_message" != *"$expected_error"* ]]; then
+  echo "Output error message: $error_message does not match expected error message: $expected_error"
+  exit 1
+fi
+
+echo "> Error test successful"
+
+echo "---- All tests succeeded! ----"
+exit 0
