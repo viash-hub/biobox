cb9316751cfbb2fa4bcf53602c87f56230f01e7d
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-19 15:15:54 +0200

    update bedtools getfasta

--- src/bedtools/bedtools_getfasta/script.sh
+++ src/bedtools/bedtools_getfasta/script.sh
@@ -1,22 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
 set -eo pipefail
 
-unset_if_false=( par_rna par_strandedness par_tab par_bed_out par_name par_name_only par_split par_full_header )
+# unset flags
+unset_if_false=(
+  par_rna
+  par_strandedness
+  par_split
+  par_full_header
+  par_name
+  par_name_only
+  par_tab
+  par_bed_out
+)
 
-for par in ${unset_if_false[@]}; do
+for par in "${unset_if_false[@]}"; do
     test_val="${!par}"
-    [[ "$test_val" == "false" ]] && unset $par
+    [[ "$test_val" == "false" ]] && unset "$par"
 done
 
-bedtools getfasta \
-    -fi "$par_input_fasta" \
-    -bed "$par_input_bed" \
-    ${par_rna:+-rna} \
-    ${par_name:+-name} \
-    ${par_name_only:+-nameOnly} \
-    ${par_tab:+-tab} \
-    ${par_bed_out:+-bedOut} \
-    ${par_strandedness:+-s} \
-    ${par_split:+-split} \
-    ${par_full_header:+-fullHeader} > "$par_output"
+# Build command arguments array
+cmd_args=(
+    -fi "$par_input_fasta"
+    -bed "$par_input_bed"
+    -fo "$par_output"
+    ${par_rna:+-rna}
+    ${par_strandedness:+-s}
+    ${par_split:+-split}
+    ${par_full_header:+-fullHeader}
+    ${par_name:+-name}
+    ${par_name_only:+-nameOnly}
+    ${par_tab:+-tab}
+    ${par_bed_out:+-bedOut}
+)
+
+# Execute bedtools command
+bedtools getfasta "${cmd_args[@]}"
 
