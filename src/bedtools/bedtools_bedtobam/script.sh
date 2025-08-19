f2c22b43190bdb260b922cb0193107f57c11707e
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-19 12:47:44 +0200

    update bedtobam

--- src/bedtools/bedtools_bedtobam/script.sh
+++ src/bedtools/bedtools_bedtobam/script.sh
@@ -5,15 +5,18 @@
 
 set -eo pipefail
 
-# Unset parameters
+# Unset false boolean parameters
 [[ "$par_bed12" == "false" ]] && unset par_bed12
 [[ "$par_uncompress_bam" == "false" ]] && unset par_uncompress_bam
 
-# Execute bedtools bed to bam 
-bedtools bedtobam \
-    ${par_bed12:+-bed12} \
-    ${par_uncompress_bam:+-ubam} \
-    ${par_map_quality:+-mapq "$par_map_quality"} \
-    -i "$par_input" \
-    -g "$par_genome" \
-    > "$par_output"
+# Build command arguments array
+cmd_args=(
+    -i "$par_input"
+    -g "$par_genome"
+    ${par_map_quality:+-mapq "$par_map_quality"}
+    ${par_bed12:+-bed12}
+    ${par_uncompress_bam:+-ubam}
+)
+
+# Execute bedtools bedtobam
+bedtools bedtobam "${cmd_args[@]}" > "$par_output"
