a27b366463b6e7ca051e7dc4d87c4425eee952b4
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-19 15:56:32 +0200

    update groubly

--- src/bedtools/bedtools_groupby/script.sh
+++ src/bedtools/bedtools_groupby/script.sh
@@ -3,34 +3,37 @@
 ## VIASH START
 ## VIASH END
 
-# Exit on error
 set -eo pipefail
 
-# Unset parameters
+# unset flags
 unset_if_false=(
-    par_full
-    par_inheader
-    par_outheader
-    par_header
-    par_ignorecase
+  par_full
+  par_inheader
+  par_outheader
+  par_header
+  par_ignorecase
 )
 
-for par in ${unset_if_false[@]}; do
+for par in "${unset_if_false[@]}"; do
     test_val="${!par}"
-    [[ "$test_val" == "false" ]] && unset $par
+    [[ "$test_val" == "false" ]] && unset "$par"
 done
 
-bedtools groupby \
-    ${par_full:+-full} \
-    ${par_inheader:+-inheader} \
-    ${par_outheader:+-outheader} \
-    ${par_header:+-header} \
-    ${par_ignorecase:+-ignorecase} \
-    ${par_precision:+-prec "$par_precision"} \
-    ${par_delimiter:+-delim "$par_delimiter"} \
-    -i "$par_input" \
-    -g "$par_groupby" \
-    -c "$par_column" \
-    ${par_operation:+-o "$par_operation"} \
-    > "$par_output"
+# Build command arguments array
+cmd_args=(
+    -i "$par_input"
+    -g "$par_groupby"
+    -c "$par_column"
+    ${par_operation:+-o "$par_operation"}
+    ${par_full:+-full}
+    ${par_inheader:+-inheader}
+    ${par_outheader:+-outheader}
+    ${par_header:+-header}
+    ${par_ignorecase:+-ignorecase}
+    ${par_precision:+-prec "$par_precision"}
+    ${par_delimiter:+-delim "$par_delimiter"}
+)
+
+# Execute bedtools command
+bedtools groupby "${cmd_args[@]}" > "$par_output"
     
