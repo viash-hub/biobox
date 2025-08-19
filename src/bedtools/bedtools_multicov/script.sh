606206b5f22b80a391a83bdd0422945006309130
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-20 00:20:37 +0200

    add bedtools multicov

---
+++ src/bedtools/bedtools_multicov/script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+set -eo pipefail
+
+# unset flags
+[[ "$par_reciprocal" == "false" ]] && unset par_reciprocal
+[[ "$par_same_strand" == "false" ]] && unset par_same_strand
+[[ "$par_opposite_strand" == "false" ]] && unset par_opposite_strand
+[[ "$par_include_duplicates" == "false" ]] && unset par_include_duplicates
+[[ "$par_include_failed_qc" == "false" ]] && unset par_include_failed_qc
+[[ "$par_proper_pairs_only" == "false" ]] && unset par_proper_pairs_only
+[[ "$par_split" == "false" ]] && unset par_split
+
+# Build command arguments array
+cmd_args=(
+    -bams "${par_bams[@]}"
+    -bed "$par_bed"
+    ${par_min_overlap:+-f "$par_min_overlap"}
+    ${par_reciprocal:+-r}
+    ${par_same_strand:+-s}
+    ${par_opposite_strand:+-S}
+    ${par_min_quality:+-q "$par_min_quality"}
+    ${par_include_duplicates:+-D}
+    ${par_include_failed_qc:+-F}
+    ${par_proper_pairs_only:+-p}
+    ${par_split:+-split}
+)
+
+# Execute bedtools multicov
+bedtools multicov "${cmd_args[@]}" > "$par_output"
