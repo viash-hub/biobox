3c7ee35e9a5c3128c2121ca6d9629305124f7deb
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-19 21:34:25 +0200

    add bedtools closest

---
+++ src/bedtools/bedtools_closest/script.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+set -eo pipefail
+
+# Unset false flags to prevent them from being passed to bedtools
+unset_if_false=(
+    par_distance
+    par_ignore_overlaps
+    par_ignore_upstream
+    par_ignore_downstream
+    par_force_upstream
+    par_force_downstream
+    par_strand
+    par_different_strand
+    par_different_names
+)
+
+for par in ${unset_if_false[@]}; do
+    test_val="${!par}"
+    [[ "$test_val" == "false" ]] && unset $par
+done
+
+# Build input B arguments array
+input_b_args=()
+for file in "${par_input_b[@]}"; do
+    input_b_args+=(-b "$file")
+done
+
+# Execute bedtools closest
+bedtools closest \
+    -a "$par_input_a" \
+    "${input_b_args[@]}" \
+    ${par_distance:+-d} \
+    ${par_distance_mode:+-D "$par_distance_mode"} \
+    ${par_ignore_overlaps:+-io} \
+    ${par_ignore_upstream:+-iu} \
+    ${par_ignore_downstream:+-id} \
+    ${par_force_upstream:+-fu} \
+    ${par_force_downstream:+-fd} \
+    ${par_strand:+-s} \
+    ${par_different_strand:+-S} \
+    ${par_k_closest:+-k "$par_k_closest"} \
+    ${par_tie_mode:+-t "$par_tie_mode"} \
+    ${par_different_names:+-N} \
+    > "$par_output"
