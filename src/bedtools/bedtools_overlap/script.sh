9ba71a8703995a36761aa411bb304721ed3693b7
Robrecht Cannoodt <rcannood@gmail.com>
2025-08-20 09:46:06 +0200

    add bedtools overlap command

---
+++ src/bedtools/bedtools_overlap/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## VIASH START
+## VIASH END
+
+set -eo pipefail
+
+# Build command arguments array
+cmd_args=(
+    -i "$par_input"
+    -cols "$par_cols"
+)
+
+# Execute bedtools overlap
+bedtools overlap "${cmd_args[@]}" > "$par_output"
